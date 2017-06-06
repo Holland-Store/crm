796fe76f2158f1d2e3a4e1c88636007b7d8edc13
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-06 17:19:38 +0300

    Вид для раскрывающего заказа. Аналог полного просмотра

--- frontend/views/zakaz/_zakaz.php
+++ frontend/views/zakaz/_zakaz.php
@@ -1,23 +1,39 @@
 <?php
 use yii\helpers\Html;
 use kartik\detail\DetailView;
+use yii\i18n\Formatter;
 
 ?>
 
 <?= DetailView::widget([
 	'model' => $model,
+	'mode' => 'view',
+	'condensed' => true,
 	'rowOptions' => ['style' => 'font-size: 11px;'],
 	'panel' => [
-		'heading' => 'Заказ',
+		'heading' => $model->description,
         'type' => DetailView::TYPE_PRIMARY,
     ],
 	'attributes' => [
 		[
-			'attribute' => data,
+					'group' => true,
+					'label' => 'Информация о заказе',
+					'rowOptions' => ['class' => 'info'],
+					'groupOptions' => ['style' => 'color:black']
+		],
+		[
+			'attribute' => information,
 			'columns' => [
+				[
+					'attribute' => 'number',
+					'labelColOptions' => ['style' => 'width:10%;color:black'],
+					'valueColOptions' => ['style' => 'width:5%;color:black'],
+				],
 				[
 					'attribute' => 'data',
 					'format' => ['date', 'd.MM.Y'],
+					'labelColOptions' => ['style' => 'width:10%;color:black'],
+					'valueColOptions' => ['style' => 'width:5%;color:black'],
 				],
 				[
 					'attribute' => 'information',
@@ -28,21 +44,131 @@ use kartik\detail\DetailView;
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+					'labelColOptions' => ['style' => 'color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
 				],
+				// [
+				// 	'attribute' => 'srok',
+				// 	'format' => ['date','d.MM.Y'],
+				// 	'labelColOptions' => ['style' => 'width:5%; color:black'],
+				// 	'valueColOptions' => ['style' => 'width:5%; color:black'],
+				// ],
 				[
-					'attribute' => 'srok',
-					'format' => ['date','d.MM.Y'],
-					'labelColOptions' => ['style' => 'width:20%'],
-					'valueColOptions' => ['style' => 'width:20%'],
+					'attribute' => 'img',
+					'format' => 'raw',
+					'value' => $model->img == null ? '' : Html::a($model->img, '@web/attachment/'.$model->img, ['download' => true, 'data-pjax' => 0]),
+					'labelColOptions' => ['style' => 'width:10%;color:black'],
+					'valueColOptions' => ['style' => 'width:5%;color:black'],
 				],
 			],
 			[
 				'attribute' => prioritetName,
 			],
 		],
+		[
+			'group' => true,
+			'label' => 'Клиент',
+					'rowOptions' => ['class' => 'info'],
+					'groupOptions' => ['style' => 'color:black'],
+
+		],
+		[
+			'attribute' => client,
+			'columns' => [
+				[
+					'attribute' => 'name',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+
+				],
+				[
+					'attribute' => 'phone',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+				[
+					'attribute' => 'email',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+			],
+		],
+		[
+			'group' => true,
+			'label' => 'Оплата',
+					'rowOptions' => ['class' => 'info'],
+					'groupOptions' => ['style' => 'color:black'],
+
+		],
+		[
+			'attribute' => oplate,
+			'columns' => [
+				// [
+				// 	'attribute' => 'oplata',
+				// 	'labelColOptions' => ['style' => 'width:5%;color:black'],
+				// 	'valueColOptions' => ['style' => 'color:black'],
+
+				// ],
+				[
+					'attribute' => 'fact_oplata',
+					'label' => 'Предоплата',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+				[
+					'attribute' => 'fact_oplata',
+					'label' => 'К доплате',
+					'value' => $model->oplata != null ? $model->oplata - $model->fact_oplata.' рублей' : '',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+			],
+		],
+		[
+			'group' => true,
+			'label' => 'Управление',
+					'rowOptions' => ['class' => 'info'],
+					'groupOptions' => ['style' => 'color:black'],
+
+		],
+		[
+			'attribute' => control,
+			'columns' => [
+				// [
+				// 	'attribute' => 'srok',
+				// 	'format' => ['date','d.MM.Y'],
+				// 	// 'formatter' => [
+				// 	// 	'class' => 'yii\i18n\Formatter',
+				// 	// 	'dateFormat' => 'php:d.m.Y',
+				// 	// 	],
+				// 	'labelColOptions' => ['style' => 'width:5%;color:black'],
+				// 	'valueColOptions' => ['style' => 'color:black'],
+
+				// ],
+				[
+					'attribute' => 'id_tovar',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+				[
+					'attribute' => 'statusName',
+					'label' => 'Этап',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+				[
+					'attribute' => 'sotrud_name',
+					'labelColOptions' => ['style' => 'width:5%;color:black'],
+					'valueColOptions' => ['style' => 'color:black'],
+				],
+			],
+		],
+		[
+			'attribute' => 'id_shipping',
+			'label' => 'Доставка',
+			'value' => $model->idShipping->dostavkaName,
+			'labelColOptions' => ['style' => 'width:5%;color:black'],
+			'valueColOptions' => ['style' => 'color:black'],
+		],
 	],
-
-
-
-
 ])  ?>
