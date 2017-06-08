c07b375e702305be1337a38979e094fc4bff3c53
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-08 17:59:11 +0300

    внутрений блок в grid

---
+++ frontend/view/zakazedit.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Custom */
+
+?>
+<div class="custom-create">
+
+    <?= $this->render('_zakazedit', [
+        'model' => $model,
+    ]) ?>
+
+</div>
