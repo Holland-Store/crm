610e11f4d87e4ce4a4b728fcfe27d387e5365caa
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-09 17:58:58 +0300

    реализован внутрений блок редактирование

--- frontend/views/zakaz/_zakazedit.php
+++ frontend/views/zakaz/_zakazedit.php
@@ -2,16 +2,41 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\Url;
+use yii\widgets\MaskedInput;
 ?>
 
+<?php $this->registerJs('
+$("body").on("click", "#view", function(){
+        var key = $(this).data("key");
+        $.ajax({
+            url: "'.Url::toRoute(['zakaz/zakazold']).'?id="+key,
+            success: function(html){
+                $(".view-zakaz").html(html);
+            }
+        })
+    });
+    $("body").on("click", ".trTable", function(){
+        var key = $(this).data("key");
+        $.ajax({
+            url: "'.Url::toRoute(['zakaz/zakazold']).'?id="+key,
+            success: function(html){
+                $(".view-zakaz").html(html);
+            }
+        })  
+    })
+    ') ?>
 <div class="edit-zakaz">
     <?php $form = ActiveForm::begin(); ?>
     <div class="col-lg-2">
         <?= $models->data ?>
         <?= $models->idSotrud->name ?>
         <?= $form->field($models, 'name')->textInput() ?>
-        <?= $form->field($models, 'phone')->textInput() ?>
-        <?= $form->field($models, 'email')->textInput(['type' => 'email']) ?>
+        <?= $form->field($models, 'phone')->widget(MaskedInput::className(),[
+            'mask' => '89999999999'
+        ]) ?>
+        <?= $form->field($models, 'email')->widget(MaskedInput::className(),[
+            'clientOptions' => ['alias' => 'email']
+        ]) ?>
     </div>
     <div class="col-lg-7">
         <?= $form->field($models, 'information')->textarea(['max' => 500]); ?>
@@ -26,6 +51,7 @@ use yii\helpers\Url;
             '8' => 'Аутсорс',
             '1' => 'Исполнен',
         ])?>
+        <?= $form->field($models, 'time')->textInput(['type' => 'number', 'min' => 0, 'max' => 60])?>
         <?= $models->idShipping->dostavkaName ?>
         <?= $models->maket ?>
     </div>
@@ -33,5 +59,5 @@ use yii\helpers\Url;
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
     </div>
     <?php ActiveForm::end(); ?>
-    <?= Html::button('Вернуться', ['id' => 'view']) ?>
+    <?= Html::button('Вернуться', ['id' => 'view', 'data-key' => $models->id_zakaz]) ?>
 </div>
