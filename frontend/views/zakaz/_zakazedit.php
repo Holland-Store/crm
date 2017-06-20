d58083397a81cda1879e85ca1ac1ffe24785ac06
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-20 11:13:15 +0300

    edit widget FileInput

--- frontend/views/zakaz/_zakazedit.php
+++ frontend/views/zakaz/_zakazedit.php
@@ -3,13 +3,14 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\Url;
 use yii\widgets\MaskedInput;
+use kartik\file\FileInput;
 ?>
 
 <?php $this->registerJs('
 $("body").on("click", "#view", function(){
         var key = $(this).data("key");
         $.ajax({
-            url: "'.Url::toRoute(['zakaz/zakazold']).'?id="+key,
+            url: "'.Url::toRoute(['zakaz/zakaz']).'?id="+key,
             success: function(html){
                 $(".view-zakaz").html(html);
             }
@@ -18,7 +19,7 @@ $("body").on("click", "#view", function(){
     $("body").on("click", ".trTable", function(){
         var key = $(this).data("key");
         $.ajax({
-            url: "'.Url::toRoute(['zakaz/zakazold']).'?id="+key,
+            url: "'.Url::toRoute(['zakaz/zakaz']).'?id="+key,
             success: function(html){
                 $(".view-zakaz").html(html);
             }
@@ -26,7 +27,9 @@ $("body").on("click", "#view", function(){
     })
     ') ?>
 <div class="edit-zakaz">
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin([
+            'options' => ['enctype' => 'multipart/form-data'],
+    ]); ?>
     <div class="col-lg-2">
         <?= $models->data ?>
         <?= $models->idSotrud->name ?>
@@ -41,7 +44,41 @@ $("body").on("click", "#view", function(){
     <div class="col-lg-7">
         <?= $form->field($models, 'information')->textarea(['max' => 500]); ?>
         <?= $form->field($models, 'number')->textInput(['type' => 'number']); ?>
-        <?= $form->field($models, 'file')->fileInput() ?>
+        <?php //$form->field($models, 'file')->fileInput() ?>
+        <?=$form->field($models, 'file')->widget(FileInput::className(), [
+            'language' => 'ru',
+            'options' => ['multiple' => false],
+            'pluginOptions' => [
+                'showCaption' => false,
+                'showRemove' => false,
+                'showUpload' => false,
+                'showPreview' => true,
+                'browseClass' => 'btn btn-success',
+                'uploadClass' => 'btn btn-info',
+                'removeClass' => 'btn btn-danger',
+                'previewFileType' => 'any',
+                'maxFileCount' => 1,
+                'autoReplace' => true,
+                'theme' => 'explorer',
+                'preferIconicPreview' => true,
+                'previewFileIconSettings' => ([
+                    'doc' => '<i class="fa fa-file-word-o text-primary"></i>',
+                    'xls' => '<i class="fa fa-file-excel-o text-success"></i>',
+                    'ppt' => '<i class="fa fa-file-powerpoint-o text-danger"></i>',
+                    'pdf' => '<i class="fa fa-file-pdf-o text-danger"></i>',
+                    'zip' => '<i class="fa fa-file-archive-o text-muted"></i>',
+                    'rar' => '<i class="fa fa-file-archive-o text-muted"></i>',
+                    'txt' => '<i class="fa fa-file-text-o text-info"></i>',
+                    'jpg' => '<i class="fa fa-file-photo-o text-danger"></i>',
+                    'png' => '<i class="fa fa-file-photo-o text-danger"></i>',
+                    'gif' => '<i class="fa fa-file-photo-o text-danger"></i>',
+                ]),
+                'layoutTemplates' => ([
+                        'actionDelete' => '',
+                        'actionUpload' => '',
+                    ]),
+            ],
+        ]) ?>
     </div>
     <div class="col-lg-3">
         <?= $form->field($models, 'status')->dropDownList([
