5f2e2c1023097688195d6e88e7ce64a33bf28748
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-22 10:51:43 +0300

    add data sampling

--- frontend/views/zakaz/_zakaz.php
+++ frontend/views/zakaz/_zakaz.php
@@ -4,6 +4,8 @@ use yii\widgets\DetailView;
 use yii\helpers\Url;
 use yii\bootstrap\Modal;
 use app\models\Courier;
+use yii\bootstrap\ActiveForm;
+use app\models\Comment;
 ?>
 
 <?php $this->registerJs('$("body").on("click", "#edit", function(){
@@ -46,8 +48,46 @@ use app\models\Courier;
         <div class="divInform">
         <?= $model->information ?>
         </div>
+        <?php $comments = Comment::find()->where(['id_zakaz' => $model->id_zakaz])->all(); ?>
         <div class="comment-zakaz">
-            <?= 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolor doloremque eos eum ex hic ipsam iste nobis obcaecati odio possimus quas quasi qui quis quo rem sapiente sequi sit sunt tenetur, ut vel voluptas. Autem cumque dolore dolorem dolores enim esse ex exercitationem hic iusto, laborum minima mollitia numquam odit pariatur, rem veritatis, voluptatum. Accusantium aliquid earum hic illum laboriosam libero, nesciunt provident quisquam repellendus vel. Accusamus architecto atque cum doloremque earum eius, eos, excepturi iusto magni maxime obcaecati quo repudiandae saepe similique voluptate voluptatem voluptatum! Alias assumenda autem dolores est facilis inventore laudantium libero maxime, neque perspiciatis, placeat porro qui quod! Beatae corporis earum enim eos, excepturi expedita laudantium maxime nam nobis obcaecati quisquam sunt suscipit tenetur, ut veritatis? Ad commodi consectetur cum cumque obcaecati rerum sint! Delectus distinctio dolorum magnam nobis quaerat recusandae ullam. Alias deleniti deserunt dolores ea eum excepturi id impedit in ipsum iure libero maxime nemo nesciunt nostrum odit officiis omnis pariatur perspiciatis quibusdam, ratione sed temporibus vero! Adipisci amet, consectetur culpa distinctio ducimus, error explicabo ipsum, iure magni molestiae molestias reiciendis similique sint totam veritatis? Amet beatae dolorum, inventore ipsa magni maiores praesentium vitae? Ab at aut beatae dicta doloremque doloribus ea eaque eligendi, esse excepturi expedita facere fugiat maiores minus modi molestiae neque nisi, nulla odit perferendis placeat porro provident quas quasi quia, quibusdam quod rem repellat repudiandae sapiente sequi similique soluta tempora tenetur vel vitae voluptas. Deleniti dicta doloremque ducimus exercitationem minus temporibus ut! Ab facere fugiat libero minima nemo optio veritatis. Accusantium ad at atque autem culpa deserunt distinctio dolor doloribus eaque, eligendi error eum explicabo id in mollitia nihil nisi officiis perferendis placeat qui quos rem repellat reprehenderit sit tempore tenetur veniam. A accusantium aliquid aperiam asperiores consequuntur corporis cupiditate debitis distinctio eius eos esse harum ipsam iste iusto libero magni maxime molestiae necessitatibus nihil nobis nulla praesentium quaerat quia, quis ratione repellat reprehenderit rerum soluta tempora tempore tenetur unde velit voluptas. Aperiam, ea error hic incidunt ipsa ipsam iure iusto laboriosam magni minima odio pariatur quibusdam quod repellendus, repudiandae similique sint, sit vitae voluptate voluptatibus? Consequuntur, dolore id maxime minima ullam veniam voluptatum! Atque autem blanditiis, consequatur ex harum id ipsum provident quo reprehenderit soluta? Aliquam aliquid autem cum dicta doloremque illo iste, maiores nesciunt non placeat possimus praesentium quam quisquam rem repellat, repellendus saepe sapiente. Culpa eveniet id ipsum labore maxime, molestiae nemo, perspiciatis possimus quia quibusdam quo sequi suscipit. Ab blanditiis dolores enim eum eveniet inventore ipsum laudantium nemo nobis nostrum quaerat, quo saepe sit. A accusamus ad adipisci architecto asperiores aut autem dolore dolorem eligendi explicabo fugit in iste iure labore libero minus molestiae natus nostrum odit provident qui quidem ratione repellendus similique veniam, voluptas voluptates voluptatibus. Cupiditate dolorum eaque error est laudantium minus mollitia officiis quibusdam. Deleniti deserunt dignissimos modi neque nesciunt porro possimus quas sunt totam voluptas? Adipisci aliquam animi aperiam asperiores blanditiis consectetur consequuntur explicabo laborum maiores modi mollitia necessitatibus nisi, numquam odit praesentium quidem soluta suscipit ullam unde vero voluptas.'?>
+            <?php  foreach ($comments as $com){
+                if ($com->id_user == Yii::$app->user->id){
+                    $user = 'Я';
+                } elseif ($com->id_user == 3){
+                    $user = 'Дизайнер';
+                } elseif ($com->id_user == 4){
+                    $user = 'Мастер';
+                }
+                echo  '<div style="display: block;height: 100%;"><div style="width: 62px;float: left;">'.$user.'</div> <div style="width: 438px;float: left;">'.$com->comment.'</div> <div style="float: left;">'.date('d.m H:i', strtotime($com->date)).'</div style="float: left;"></div>';
+            }?>
+        </div>
+        <div>
+            <?php $formComment = ActiveForm::begin([
+                    'id' => 'formComment',
+            ]); ?>
+            <?php if ($model->status == 3){
+                $comment->sotrud = 3;
+                $sotrud = $comment->sotrud;
+            } elseif($model->status == 4){
+                $comment->sotrud = 4;
+                $sotrud = $comment->sotrud;
+            }
+            ?>
+            <div class="col-lg-11">
+                <?= $formComment->field($comment, 'comment')->textarea(['placeholder' => 'Комментарий', 'rows' => 1])->label(false) ?>
+                <?= $formComment->field($comment, 'id_user')->hiddenInput(['value' => Yii::$app->user->getId()])->label(false)?>
+                <?= $formComment->field($comment, 'sotrud')->hiddenInput(['value' => $sotrud])->label(false)?>
+                <?= $formComment->field($comment, 'id_zakaz')->hiddenInput(['value' => $model->id_zakaz])->label(false)?>
+            </div>
+            <div class="col-lg-1">
+                <?= Html::submitButton(' <span class="glyphicon glyphicon-send"></span>', ['class' => 'btn btn-primary', 'style' => '    color: white;
+    font-size: 15px;
+    margin-top: 0px;
+    margin-left: -30px;
+    border-radius: 45px;'])?>
+
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
 		<?= Detailview::widget([
 			'model' => $model,
