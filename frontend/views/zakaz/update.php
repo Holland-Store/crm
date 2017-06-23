2858ab3d4772c618f5eeb2f71ca1464dff8ce893
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-23 10:29:32 +0300

    removed sandbox

--- frontend/views/zakaz/update.php
+++ frontend/views/zakaz/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Zakaz */
 
-$this->title = 'Редактировать заказ: ' . $model->id_zakaz;
-$this->params['breadcrumbs'][] = ['label' => 'Заказ', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id_zakaz, 'url' => ['view', 'id' => $model->id_zakaz]];
-$this->params['breadcrumbs'][] = 'Редактировать';
+//$this->title = 'Редактировать заказ: ' . $model->id_zakaz;
+//$this->params['breadcrumbs'][] = ['label' => 'Заказ', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->id_zakaz, 'url' => ['view', 'id' => $model->id_zakaz]];
+//$this->params['breadcrumbs'][] = 'Редактировать';
 ?>
 <div class="zakaz-update">
 
