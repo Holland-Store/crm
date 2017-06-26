3f9ad3b48b4c7b58602e8c574aa6c890d0331abd
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-26 10:06:36 +0300

    commit title

--- frontend/views/zakaz/update.php
+++ frontend/views/zakaz/update.php
@@ -5,14 +5,14 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Zakaz */
 
-//$this->title = 'Редактировать заказ: ' . $model->id_zakaz;
-//$this->params['breadcrumbs'][] = ['label' => 'Заказ', 'url' => ['index']];
-//$this->params['breadcrumbs'][] = ['label' => $model->id_zakaz, 'url' => ['view', 'id' => $model->id_zakaz]];
-//$this->params['breadcrumbs'][] = 'Редактировать';
+$this->title = 'Редактировать заказ: ' . $model->id_zakaz;
+$this->params['breadcrumbs'][] = ['label' => 'Заказ', 'url' => ['admin']];
+$this->params['breadcrumbs'][] = ['label' => $model->id_zakaz, 'url' => ['admin', '#' => $model->id_zakaz]];
+$this->params['breadcrumbs'][] = 'Редактировать';
 ?>
 <div class="zakaz-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<!--    <h1>--><?//= Html::encode($this->title) ?><!--</h1>-->
 
     <?= $this->render('_form', [
         'model' => $model,
