f0fa9ce06269dfb67d6efaa26613b395b804be99
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-26 10:09:30 +0300

    add new STATUS_DECLINED_DISAIN and STATUS_DECLINED_MASTER. Transformation file _zakaz.php

--- frontend/views/zakaz/_zakaz.php
+++ frontend/views/zakaz/_zakaz.php
@@ -9,16 +9,16 @@ use app\models\Comment;
 use app\models\Zakaz;
 ?>
 
-<?php $this->registerJs('$("body").on("click", "#edit", function(){
-           var key = $(this).data("key");
-        $.ajax({
-            url: "'.Url::toRoute(['zakaz/zakazedit']).'?id="+key,
-            timeout: 10000,
-            success: function(html){
-                $(".view-zakaz").html(html);
-            }
-        })
-    });') ?>
+<?php //$this->registerJs('$("body").on("click", "#edit", function(){
+//           var key = $(this).data("key");
+//        $.ajax({
+//            url: "'.Url::toRoute(['zakaz/zakazedit']).'?id="+key,
+//            timeout: 10000,
+//            success: function(html){
+//                $(".view-zakaz").html(html);
+//            }
+//        })
+//    });') ?>
 
 <div class="view-zakaz" style="color: black">
 	<div class="col-lg-2 anketaZakaz">
@@ -36,7 +36,7 @@ use app\models\Zakaz;
     </div>
 	<div class="col-lg-7 zakazInfo">
         <div class="divInform">
-        <?= 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quia quaerat, id velit sit, perspiciatis repudiandae repellendus, distinctio ut, qui illo veniam! Quisquam blanditiis nostrum commodi esse inventore est eaque quidem rerum neque expedita ipsum itaque delectus saepe quia voluptatibus, aspernatur necessitatibus placeat rem minima facilis, dolores odio. Officiis voluptas, minima error iusto magnam omnis nostrum esse autem, sapiente sed repudiandae, natus quibusdam? Nisi fugit ex itaque animi cumque distinctio veritatis tempora possimus cupiditate culpa quam incidunt molestiae, minus laborum dolorum quidem aliquam vel. Quae nisi eveniet nam a consequuntur quaerat veniam sit rerum pariatur. Praesentium laborum sunt dolor, non. Enim error praesentium hic, mollitia deserunt id a iure at, in delectus, aspernatur nisi sint maiores illo animi ratione maxime porro impedit accusantium. Perspiciatis consequatur cum distinctio esse eaque repellat velit qui neque nam sed id non explicabo accusamus cupiditate eveniet, sunt officiis temporibus officia nesciunt porro. Earum aut deleniti adipisci vero explicabo sit et eum ad iste animi distinctio soluta, ipsam blanditiis veniam illum temporibus sint, commodi, laborum itaque cum ullam nihil reprehenderit laudantium consequatur debitis! Dolorem, debitis, quaerat. Eligendi eum officiis excepturi cupiditate dolorum, provident perferendis sint voluptatem natus aliquam fugit tempora earum alias atque iste impedit. Laudantium quas tempora quae vero eaque necessitatibus nam sint doloremque dolorum voluptas cupiditate soluta perferendis quibusdam animi, minus magnam minima ullam. Pariatur incidunt totam sed tenetur explicabo, recusandae quidem, placeat blanditiis at repellendus ipsum minima quia et, dolor animi aperiam tempore voluptates provident fuga qui. Nostrum perspiciatis vitae maiores consequuntur iure sed est repellat natus minima placeat saepe, amet eius blanditiis, facilis doloribus a cum cumque aspernatur porro ex nobis corrupti accusamus sint possimus beatae. Delectus nemo incidunt, laboriosam, nostrum voluptatum aperiam perspiciatis voluptatem libero quidem sunt autem! Deserunt vitae, molestias soluta delectus voluptatibus atque magni consequatur est sunt! Eveniet quia dolores quas provident, sit eligendi totam incidunt nesciunt accusantium fugiat asperiores id ad magni nobis reprehenderit explicabo earum consequatur error soluta repellat, ex! Explicabo officiis dolores voluptatem libero, cum quis similique doloremque reprehenderit tempora totam mollitia esse, aliquam quibusdam commodi quos. Cupiditate atque minima nulla illum velit. Quam similique in ea nisi quis modi vero libero repellendus iure sapiente enim amet impedit maxime fugiat ullam esse ad voluptate, culpa at illum provident aspernatur natus. Rerum minima in impedit quia voluptatum necessitatibus vero hic temporibus nam reiciendis. Illum, quae. Tenetur quod consequatur vitae natus veniam in iusto blanditiis perferendis cum quos ratione unde accusantium, assumenda nulla, velit voluptate magni fuga consectetur error hic sapiente animi! Perferendis atque recusandae, alias sequi quos voluptatem est provident, aliquid architecto. Repellat, cumque inventore incidunt, est saepe aut iure maiores amet minima debitis consequatur, voluptates accusantium enim reiciendis corporis veniam a eveniet. Blanditiis illum ducimus ipsa magnam quam sint ab fuga et, alias, architecto laborum itaque nostrum. Rem repellat voluptatem culpa distinctio provident iusto molestiae porro autem nostrum quibusdam aliquid veniam fuga blanditiis, quos quae asperiores ab dolor, quis in ad eum, ut voluptates numquam! Dolor aut quos, blanditiis accusantium molestias, tempora labore cupiditate quidem amet?' 
+        <?= 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quia quaerat, id velit sit, perspiciatis repudiandae repellendus, distinctio ut, qui illo veniam! Quisquam blanditiis nostrum commodi esse inventore est eaque quidem rerum neque expedita ipsum itaque delectus saepe quia voluptatibus, aspernatur necessitatibus placeat rem minima facilis, dolores odio. Officiis voluptas, minima error iusto magnam omnis nostrum esse autem, sapiente sed repudiandae, natus quibusdam? Nisi fugit ex itaque animi cumque distinctio veritatis tempora possimus cupiditate culpa quam incidunt molestiae, minus laborum dolorum quidem aliquam vel. Quae nisi eveniet nam a consequuntur quaerat veniam sit rerum pariatur. Praesentium laborum sunt dolor, non. Enim error praesentium hic, mollitia deserunt id a iure at, in delectus, aspernatur nisi sint maiores illo animi ratione maxime porro impedit accusantium. Perspiciatis consequatur cum distinctio esse eaque repellat velit qui neque nam sed id non explicabo accusamus cupiditate eveniet, sunt officiis temporibus officia nesciunt porro. Earum aut deleniti adipisci vero explicabo sit et eum ad iste animi distinctio soluta, ipsam blanditiis veniam illum temporibus sint, commodi, laborum itaque cum ullam nihil reprehenderit laudantium consequatur debitis! Dolorem, debitis, quaerat. Eligendi eum officiis excepturi cupiditate dolorum, provident perferendis sint voluptatem natus aliquam fugit tempora earum alias atque iste impedit. Laudantium quas tempora quae vero eaque necessitatibus nam sint doloremque dolorum voluptas cupiditate soluta perferendis quibusdam animi, minus magnam minima ullam. Pariatur incidunt totam sed tenetur explicabo, recusandae quidem, placeat blanditiis at repellendus ipsum minima quia et, dolor animi aperiam tempore voluptates provident fuga qui. Nostrum perspiciatis vitae maiores consequuntur iure sed est repellat natus minima placeat saepe, amet eius blanditiis, facilis doloribus a cum cumque aspernatur porro ex nobis corrupti accusamus sint possimus beatae. Delectus nemo incidunt, laboriosam, nostrum voluptatum aperiam perspiciatis voluptatem libero quidem sunt autem! Deserunt vitae, molestias soluta delectus voluptatibus atque magni consequatur est sunt! Eveniet quia dolores quas provident, sit eligendi totam incidunt nesciunt accusantium fugiat asperiores id ad magni nobis reprehenderit explicabo earum consequatur error soluta repellat, ex! Explicabo officiis dolores voluptatem libero, cum quis similique doloremque reprehenderit tempora totam mollitia esse, aliquam quibusdam commodi quos. Cupiditate atque minima nulla illum velit. Quam similique in ea nisi quis modi vero libero repellendus iure sapiente enim amet impedit maxime fugiat ullam esse ad voluptate, culpa at illum provident aspernatur natus. Rerum minima in impedit quia voluptatum necessitatibus vero hic temporibus nam reiciendis. Illum, quae. Tenetur quod consequatur vitae natus veniam in iusto blanditiis perferendis cum quos ratione unde accusantium, assumenda nulla, velit voluptate magni fuga consectetur error hic sapiente animi! Perferendis atque recusandae, alias sequi quos voluptatem est provident, aliquid architecto. Repellat, cumque inventore incidunt, est saepe aut iure maiores amet minima debitis consequatur, voluptates accusantium enim reiciendis corporis veniam a eveniet. Blanditiis illum ducimus ipsa magnam quam sint ab fuga et, alias, architecto laborum itaque nostrum. Rem repellat voluptatem culpa distinctio provident iusto molestiae porro autem nostrum quibusdam aliquid veniam fuga blanditiis, quos quae asperiores ab dolor, quis in ad eum, ut voluptates numquam! Dolor aut quos, blanditiis accusantium molestias, tempora labore cupiditate quidem amet?'
 
         //$model->information ?>
         </div>
@@ -65,7 +65,7 @@ use app\models\Zakaz;
             'style' => '    padding-left: 13px;
                             padding-right: 13px;
                             float: right;
-                            margin-top: -24px;
+                            margin-top: -10px;
                             margin-right: 18px;
                             font-size: 11px;
                             background: #3a3331;
@@ -99,68 +99,77 @@ use app\models\Zakaz;
 <!--            </div>-->
 <!--            --><?php //ActiveForm::end(); ?>
 <!--        </div>-->
-		<?= Detailview::widget([
-			'model' => $model,
-			'options' => ['class' => 'table detail-view'],
-			'template' => '<tr style="color:black;border: none;"><td{contentOptions} class="zakaz-view-kartik">{value}</td></tr></tr>',
-			'attributes' => [
-				'number',
-//				'id_tovar',
-			],
-		]) ?>
 	</div>
-	<div class="col-lg-3">
+	<div class="col-lg-1 zakazFile">
+        <div class="zakazFile_block">
+            <span class="zakazFile_block-number">Кол-во:</span>
+            <div><?= $model->number ?></div>
+        </div>
 		<?= Detailview::widget([
 			'model' => $model,
 			'options' => ['class' => 'table detail-view'],
 			'template' => '<tr style="color:black;border: none;"><td{contentOptions} class="zakaz-view-kartik">{value}</td></tr></tr>',
 			'attributes' => [
-				'statusName',
-				[
-					'attribute' => 'id_shipping',
-					'value' => $model->idShipping->dostavkaName,
-				],
+                [
+                    'attribute' => 'maket',
+                    'format' =>'raw',
+                    'value' => $model->maket == null ? '<div style="margin-top: 44px"></div>' : Html::a('<span class="glyphicon glyphicon-saved imgZakaz" style="margin-top: 4px;margin-left: -33px;">', '@web/attachment/'.$model->img, ['download' => true, 'data-pjax' => 0, 'title' => 'Готовый макет от дизайнера'])
+                ],
 				[
 				    'attribute' => 'img',
                     'format' =>'raw',
-                    'value' => $model->img == null ? null : Html::a('<span class="glyphicon glyphicon-paperclip imgZakaz"></span>', '@web/attachment/'.$model->img, ['download' => true, 'data-pjax' => 0])
+                    'value' => $model->img == null ? '<div></div>' : Html::a('<span class="glyphicon glyphicon-paperclip imgZakaz" style="margin-top: -10px;margin-left: -33px;"></span>', '@web/attachment/'.$model->img, ['download' => true, 'data-pjax' => 0, 'title' => 'Исходный файл от клиента'])
 				],
-				'maket',
 			],
 		]) ?>
 	</div>
+    <div class="responsible">
+        <span class="responsible_person">Статус:</span>
+        <div>
+            <?php if ($model->status == Zakaz::STATUS_SUC_DISAIN){
+                echo '<div class="statusZakaz">Выполнено</div>
+<div>'
+                    .Html::a('<div class="action actionApprove" style="width:70px;position: relative;left: 62px;top: -1px;">Принять</div>', ['#']).' '
+                    .Html::a('<div class="action actionCancel" style="left:143px;top:-21px;width:79px;position: relative;">Отклонить</div>', ['#']).'
+</div>';
+            }
+            elseif($model->status == Zakaz::STATUS_DECLINED_DISAIN){
+                echo '<div class="statusZakaz" style="background: #7c1111">Отклонено</div>
+<div style="width: 155px;position: relative;left: 63px;top: -1px;"">
+<span class="responsible_person">По причине:</span><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, vero.</div>';
+            }
+            ?>
+        </div>
+        <div class="oplata-zakaz">
+            <span class="responsible_person">Всего:</span>
+            <span class="responsible_person">Оплачено:</span>
+            <span class="responsible_person">К доплате:</span><br>
+            <?= $model->oplata.'р.' ?>
+            <?= $model->fact_oplata.'р.' ?>
+            <?php if($model->oplata != null){?>
+                <?php echo $model->oplata - $model->fact_oplata.'р.'; ?>
+            <?php } ?>
+        </div>
+    </div>
+    <div class="col-lg-12 footerView"></div>
     <div class="col-lg-12 footer-view-zakaz">
-        <?= Html::a('Задача', ['todoist/createzakaz', 'id_zakaz' => $model->id_zakaz]) ?>
-        <?= Html::a('Запрос', ['todoist/create_shop']) ?>
+        <?= Html::a('Задача', ['todoist/createzakaz', 'id_zakaz' => $model->id_zakaz], ['class' => 'btn btn-xs', 'style' => 'margin-left: 97px;']) ?>
+        <?= Html::a('Запрос', ['todoist/create_shop'], ['class' => 'btn btn-xs']) ?>
         <?php Modal::begin([
             'header' => '<h2>Создание доставки</h2>',
             'toggleButton' => [
                 'tag' => 'a',
+                'class' => 'btn btn-xs',
                 'label' => 'Доставка',
             ]
         ]);
         $shipping = new Courier();
         echo $this->render('shipping', [
-           'shipping' => $shipping,
+            'shipping' => $shipping,
             'model' => $model->id_zakaz,
         ]);
 
         Modal::end(); ?>
-        <?php Modal::begin([
-            'header' => '<h1>№ '.$model->id_zakaz.' '.$model->description.'</h1>',
-            'size' => 'modal-lg',
-            'toggleButton' => [
-                'tag' => 'button',
-                'class' => 'btn btn-xs btn-primary',
-                'label' => 'Редактировать',
-            ]
-        ]);
-
-        echo $this->render('update', [
-                'model' => $model,
-        ]) ;
-
-        Modal::end(); ?>
-        <?php //Html::buttonInput('Редактировать', ['id' => 'edit', 'data-key' => $model->id_zakaz]) ?>
+        <?= Html::a('Редактировать', ['zakaz/update', 'id' => $model->id_zakaz], ['class' => 'btn btn-xs', 'style' => 'float: right;margin-right: 10px;'])?>
     </div>
 </div>
