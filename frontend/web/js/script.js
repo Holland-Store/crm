fd7d8d63eae102352828639d53ce51ad6664d190
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-29 16:33:05 +0300

    status have change and open modal windows

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -13,4 +13,27 @@ $(document).ready(function(){
 	// setInterval(function(){
 	// 	$.pjax.reload('#pjax-container')
 	// }, 100000);
+       $('.actionCancel').click(function () {
+           $('#declinedModal').modal('show')
+               .find('.modalContent')
+               .load($(this).attr('value'));
+       });
+       $('.actionApprove').click(function () {
+           $('#acceptdModal').modal('show')
+               .find('.modalContent')
+               .load($(this).attr('value'));
+       });
+        $('.modalDisain').click(function () {
+            $('#modalFile').modal('show')
+                .find('.modalContent')
+                .load($(this).attr('value'));
+        });
+       $("body").on("click", ".trNew", function () {
+           var data = $(this).data("key");
+           $.ajax({
+               url: "/frontend/web/zakaz/adopted?id="+data,
+               success: console.log('Успешно изменен статус')
+           })
+       })
 });
+
