825e5c5d661c408d3e39161ce5ef467549bdb5dd
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-06-29 16:31:47 +0300

    transformated gridview

--- frontend/views/todoist/shop.php
+++ frontend/views/todoist/shop.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 use yii\bootstrap\Nav;
 
 /* @var $this yii\web\View */
@@ -10,51 +10,48 @@ use yii\bootstrap\Nav;
 
 $this->title = 'Задачник';
 ?>
-<div class="todoist-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<div class="todoist-index ispolShop">
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
+        'floatHeader' => true,
+        'headerRowOptions' => ['class' => 'headerTable'],
+        'pjax' => true,
+        'tableOptions' 	=> ['class' => 'table table-bordered tableSize'],
+        'rowOptions' => ['class' => 'trTable trNormal'],
+        'striped' => false,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
             [
                 'attribute' => 'srok',
-                'format' => ['date', 'php:d.m.Y'],
+                'format' => ['date', 'php:d M'],
+                'hAlign' => GridView::ALIGN_RIGHT,
+                'contentOptions' => ['class' => 'border-left textTr tr90', 'style' => 'border:none'],
             ],
-            // 'id_zakaz',
-            'status',
-            'typ',
             [
-				'attribute' => 'id_user',
-				'value' => function($model){
-					return $model->idUser->name;
-				}
-			],
-            'comment:ntext',
+                'attribute' => 'comment',
+                'contentOptions'=>['style'=>'white-space: normal;'],
+            ],
             [
                 'attribute' => 'zakaz',
                 'format' => 'raw',
                 'value' => function($model){
                     if ($model->id_zakaz != null) {
                         return Html::a($model->idZakaz->prefics, ['zakaz/view', 'id' => $model->id_zakaz]);
-                    } 
+                    }
                     return '';
                 },
                 'label' => 'Заказ',
+                'hAlign' => GridView::ALIGN_RIGHT,
+                'contentOptions' => ['class' => 'textTr tr50'],
             ],
-			[
-				'attribute' => '',
-				'format' => 'raw',
-				'value' => function($model){
-						return Html::a('Выполнить', ['close', 'id' => $model->id]);
-				}
-			]
-
-
-//            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'attribute' => '',
+                'format' => 'raw',
+                'contentOptions' => ['class' => 'border-right textTr tr50'],
+                'value' => function($model){
+                    return Html::a('Выполнить', ['close', 'id' => $model->id]);
+                }
+            ]
         ],
     ]); ?>
 </div>
