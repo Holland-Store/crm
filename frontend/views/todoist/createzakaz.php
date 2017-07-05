4c1a6befbec03d7ad5f2046a600f5fe34e69b7d7
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-05 11:36:49 +0300

    rename title and deleted use helpers

--- frontend/views/todoist/createzakaz.php
+++ frontend/views/todoist/createzakaz.php
@@ -1,12 +1,10 @@
 <?php
 
-use yii\helpers\Html;
-
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Todoist */
 
-$this->title = 'Добавить задачу к заказу '.Yii::$app->request->get('id_zakaz');
+$this->title = 'Задача к заказу '.Yii::$app->request->get('id_zakaz');
 // $this->params['breadcrumbs'][] = ['label' => 'Todoists', 'url' => ['index']];
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -14,7 +12,6 @@ $this->title = 'Добавить задачу к заказу '.Yii::$app->reque
 
     <?= $this->render('_formzakaz', [
         'model' => $model,
-
     ]) ?>
 
 </div>
