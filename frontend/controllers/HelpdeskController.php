b878c0f52730d53bd88284067e4b5f29fb69e264
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-05 12:08:49 +0300

    add the counted for the navbar to the templayte

--- frontend/controllers/HelpdeskController.php
+++ frontend/controllers/HelpdeskController.php
@@ -2,6 +2,10 @@
 
 namespace frontend\controllers;
 
+use app\models\Courier;
+use app\models\Custom;
+use app\models\Todoist;
+use app\models\Zakaz;
 use Yii;
 use app\models\Helpdesk;
 use app\models\HelpdeskSearch;
@@ -60,12 +64,31 @@ class HelpdeskController extends Controller
         $searchModel = new HelpdeskSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         $notification = $this->findNotification();
+        $score = $this->findScorezakaz('admin');
+        $scoreTodoist  = $this->findScoretodoist('admin');
+        if (Yii::$app->user->can('system')){
+            $scoreHelp = $this->findScorehelp('system');
+        } else {
+            $scoreHelp = $this->findScorehelp('adop');
+        }
+        if (Yii::$app->user->can('zakup')){
+            $scoreCustom = $this->findScorecustom('zakup');
+        } else {
+            $scoreCustom = $this->findScorecustom('adop');
+        }
+        $scoreShipping = $this->findScoreshipping();
+
 
 
         return $this->render('index', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
             'notification' => $notification,
+            'score' => $score,
+            'scoreTodoist' => $scoreTodoist,
+            'scoreHelp' => $scoreHelp,
+            'scoreCustom' => $scoreCustom,
+            'scoreShipping' => $scoreShipping,
         ]);
     }
 
@@ -93,6 +116,15 @@ class HelpdeskController extends Controller
     {
         $model = new Helpdesk();
         $notification = $this->findNotification();
+        $score = $this->findScorezakaz('admin');
+        $scoreTodoist  = $this->findScoretodoist('admin');
+        $scoreHelp = $this->findScorehelp('adop');
+        if (Yii::$app->user->can('zakup')){
+            $scoreCustom = $this->findScorecustom('zakup');
+        } else {
+            $scoreCustom = $this->findScorecustom('adop');
+        }
+        $scoreShipping = $this->findScoreshipping();
 
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             if($model->save()){
@@ -104,6 +136,11 @@ class HelpdeskController extends Controller
         return $this->render('create', [
             'model' => $model,
             'notification' => $notification,
+            'score' => $score,
+            'scoreTodoist' => $scoreTodoist,
+            'scoreHelp' => $scoreHelp,
+            'scoreCustom' => $scoreCustom,
+            'scoreShipping' => $scoreShipping,
         ]);
     }
 
@@ -185,5 +222,87 @@ class HelpdeskController extends Controller
 
         $this->view->params['notifications'] = $notification->all();
         $this->view->params['count'] =  $notifications;
+    }
+
+    /**
+     * Finds the Zakaz model based on its role value
+     * @param $role
+     * @return int|string
+     */
+    protected function findScorezakaz($role)
+    {
+        $score = Zakaz::find();
+
+        switch ($role) {
+            case 'admin':
+                return $this->view->params['scoreZakaz'] = $score->andWhere(['action' => 1])->count();
+                break;
+            case 'shop':
+                return $this->view->params['scoreZakaz'] = $score->andWhere(['id_sotrud' => Yii::$app->user->id, 'action' => 1])->count();
+                break;
+            case 'disain':
+                return $this->view->params['scoreDisain'] = $score->andWhere(['status' => [Zakaz::STATUS_DISAIN, Zakaz::STATUS_SUC_DISAIN, Zakaz::STATUS_DECLINED_DISAIN], 'action' => 1])->count();
+                break;
+            case 'master':
+                return $this->view->params['scoreMaster'] = $score->andWhere(['status' => [Zakaz::STATUS_MASTER, Zakaz::STATUS_SUC_MASTER, Zakaz::STATUS_DECLINED_MASTER], 'action' => 1])->count();
+                break;
+        }
+    }
+
+    /**
+     * Finds the Todoist model based on its role value
+     * @param $role
+     * @return int|string
+     */
+    protected function findScoretodoist($role)
+    {
+        $score = Todoist::find();
+
+        switch ($role) {
+            case 'admin':
+                return $this->view->params['scoreTodoist'] = $score->andWhere(['activate' => 0])->count();
+                break;
+            case 'adop':
+                return $this->view->params['scoreTodoist'] = $score->andWhere(['id_user' => Yii::$app->user->id, 'activate' => 0])->count();
+                break;
+        }
+    }
+
+    /**
+     * Finds the Helpdesk model
+     * @param $role
+     */
+    protected function findScorehelp($role)
+    {
+        $score = Helpdesk::find();
+
+        if ($role == 'system'){
+            $this->view->params['scoreHelp'] = $score->andWhere(['status' => 0])->count();
+        } else {
+            $this->view->params['scoreHelp'] = $score->andWhere(['id_user' => Yii::$app->user->id, 'status' => 0])->count();
+        }
+    }
+
+    /**
+     * Finds the Custom model
+     */
+    protected function findScorecustom($role)
+    {
+        $score = Custom::find();
+        if ($role == 'zakup'){
+            $this->view->params['scoreCustom'] = $score->andWhere(['action' => 0])->count();
+        } else {
+            $this->view->params['scoreCustom'] = $score->andWhere(['id_user' => Yii::$app->user->id, 'action' => 0])->count();
+        }
+
+    }
+
+    /**
+     * Finds the Courier model
+     */
+    protected function findScoreshipping()
+    {
+        $score = Courier::find();
+        $this->view->params['scoreShipping'] = $score->andWhere(['<','status', Courier::DELIVERED])->count();
     }
 }
