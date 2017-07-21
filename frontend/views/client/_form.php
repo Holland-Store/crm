e5199f042151abda14e9dce060d3dd26e4beac74
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-21 14:47:08 +0300

    add field street, home, apartament, and widget MaskedInput

--- frontend/views/client/_form.php
+++ frontend/views/client/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\client\Client */
@@ -14,14 +15,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'fio')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'phone')->widget(MaskedInput::className(),[
+        'mask' => '89999999999',
+    ]) ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'email')->widget(MaskedInput::className(), [
+            'clientOptions' => ['alias' => 'email'],
+    ]) ?>
 
-    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'street')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'home')->textInput(['type' => 'number', 'min' => 0]) ?>
+
+    <?= $form->field($model, 'apartment')->textInput(['type' => 'number', 'min' => 0]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Редактировать', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
