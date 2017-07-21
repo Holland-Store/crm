a3d8f0de08e34d2eb8a35043d73bfa32670ff979
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-21 14:50:27 +0300

    title renamed name, bradcrumbs label renamed 'Клиент', button renamed 'Редактировать' and 'Удалить'. Expanded attribute address

--- frontend/views/client/view.php
+++ frontend/views/client/view.php
@@ -6,8 +6,8 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\client\Client */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Clients', 'url' => ['index']];
+$this->title = $model->fio;
+$this->params['breadcrumbs'][] = ['label' => 'Клиент', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="client-view">
@@ -15,11 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Вы уверены, что хотите удалить клиента',
                 'method' => 'post',
             ],
         ]) ?>
@@ -27,12 +27,24 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= DetailView::widget([
         'model' => $model,
+        'options' => ['class' => 'table detail-view'],
         'attributes' => [
             'id',
             'fio',
             'phone',
             'email:email',
-            'address',
+            [
+                'attribute' => 'address',
+                'value' => function($model){
+                    if ($model->street == null){
+                        return '';
+                    } elseif($model->apartment == null){
+                        return $model->street.' д.'.$model->home;
+                    } else {
+                        return $model->street.' д.'.$model->home.' кв.'.$model->apartment;
+                    }
+                }
+            ],
         ],
     ]) ?>
 
