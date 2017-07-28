cfceae098296a6bbb9c6d496c23633af7afe8cae
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-28 14:12:19 +0300

    deleted extra

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -2,14 +2,6 @@
 // 	document.getElementById('notification-container').classList.toggle('hidden');
 // }
 $(document).ready(function(){
-//	$('body').on('click', '.trTable', function(){
-//		var key = $(this).data('key');
-//		document.location.href = "http://crm/frontend/web/view/"+key;
-//	});
-//	$('body').on('click', '#trNew', function(){
-//		var key = $(this).data('key');
-//		document.location.href = "http://crm/frontend/web/zakaz/adopted?id="+key;
-//	});
 // 	setInterval(function(){
 // 		$.pjax.reload('#pjax-container')
 // 	}, 100000);
