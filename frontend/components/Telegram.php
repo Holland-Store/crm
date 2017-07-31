bd35d8c5dc2abfe191c863b0d112e0b22911eae0
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 17:20:25 +0300

    create file Telegram.php

---
+++ frontend/components/Telegram.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace frontend\components;
+
+$token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
+$webSite = 'https://api.telegram.org/bot'.$token.'/setWebnook?url=@frontend/components/Telegram.php';
+echo $webSite;
