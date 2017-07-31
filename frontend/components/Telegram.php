c070f85b099f2cafeeaf5fe45b598565a326a7c8
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 19:19:06 +0300

    test

--- frontend/components/Telegram.php
+++ frontend/components/Telegram.php
@@ -8,7 +8,7 @@ use app\models\User;
 $output = file_get_contents('php://input');
 $message = $output['message'];
 $id = $message['chat']['id'];
-file_put_contents('logs.txt', $id);
+file_put_contents('logs.json', $id);
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
 
 $model = User::findOne(\Yii::$app->user->id);
