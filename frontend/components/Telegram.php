f9c9df702046182334381d8aa14f3e945e1c3551
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 17:27:57 +0300

    deleted echo

--- frontend/components/Telegram.php
+++ frontend/components/Telegram.php
@@ -4,4 +4,4 @@ namespace frontend\components;
 
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
 $webSite = 'https://api.telegram.org/bot'.$token.'/setWebnook?url=@frontend/components/Telegram.php';
-echo $webSite;
+
