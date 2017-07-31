d0354edd626d08d4a8d992383a5e9ec2c154477d
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 18:02:34 +0300

    rename in $id

--- frontend/components/Telegram.php
+++ frontend/components/Telegram.php
@@ -6,11 +6,10 @@ use app\models\User;
 
 
 $output = json_decode(file_get_contents('php://input'), true);
-file_put_contents('logs.txt', $output);
-$token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
-
 $message = $output['message'];
 $id = $message['chat']['id'];
+file_put_contents('logs.txt', $id);
+$token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
 
 $model = User::findOne(\Yii::$app->user->id);
 $model->telegram_chat_id = $id;
