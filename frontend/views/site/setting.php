61f98e70f8ff58c9ca79150a8172b5c194901b41
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 18:59:20 +0300

    cteate file setting

--- frontend/views/site/setting.php
+++ frontend/views/site/setting.php
@@ -5,7 +5,7 @@ use app\models\User;
 $output = file_get_contents('php://input');
 $message = $output['message'];
 $id = $message['chat']['id'];
-file_put_contents('logs.txt', $id);
+file_put_contents(dirname(dirname(__DIR__)).'components/logs.txt', $id);
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
 
 $model = User::findOne(\Yii::$app->user->id);
