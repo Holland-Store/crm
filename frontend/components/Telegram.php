d722dcb2d6f0182b501e85067a7ade3235c3edb2
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 17:54:13 +0300

    add User models and chat_id

--- frontend/components/Telegram.php
+++ frontend/components/Telegram.php
@@ -2,6 +2,19 @@
 
 namespace frontend\components;
 
+use app\models\User;
+
+
+$output = json_decode(file_get_contents('php://input'), true);
+file_put_contents('logs.txt', $output);
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
-$webSite = 'https://api.telegram.org/bot'.$token.'/setWebnook?url=@frontend/components/Telegram.php';
+
+$message = $output['message'];
+$id = $message['chat']['id'];
+
+$model = User::findOne(\Yii::$app->user->id);
+$model->telegram_chat_id = $id;
+$model->save();
+
+$webSite = 'https://api.telegram.org/bot'.$token;
 
