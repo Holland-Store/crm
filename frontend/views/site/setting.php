b54a8e208eaa3fddfdb80c305451de3a2fe2dcee
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-08-03 16:25:45 +0300

    no comment

--- frontend/views/site/setting.php
+++ frontend/views/site/setting.php
@@ -1,16 +1,18 @@
 <?php
+
+use aki\telegram\Telegram;
 use app\models\User;
 
+$bot = new Telegram();
 
-$output = file_get_contents('php://input');
-$message = $output['message'];
-$id = $message['chat']['id'];
-file_put_contents('../../components/logs.txt', $id);
+$content = json_decode(file_get_contents('php://input'));
+$message = $content['callback_query']['message'];
+$chatId = $message['chat']['id'];
+file_put_contents('logs.txt', $chatId);
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
 
 $model = User::findOne(\Yii::$app->user->id);
-$model->telegram_chat_id = $id;
+$model->telegram_chat_id = $chatId;
 $model->save();
 
 $webSite = 'https://api.telegram.org/bot'.$token;
-echo \yii\helpers\Html::a('Telegram', 'https://telegram.me/HollandSotrudbot');
