5223a164f7540022043e8340b813ad67f6fda0dc
Ruslan Absalyamov <kazan1811>
2017-08-21 15:54:50 +0300

    add alias

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,4 +3,7 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@vendor', dirname(dirname(__DIR__)) . '/vendor');
 Yii::setAlias('@npm', dirname(dirname(__DIR__)) . '@vendor/npm-asset');
+Yii::setAlias('@YiiNodeSocket', '@vendor/ratacibernetica/yii2-node-socket/lib/php');
+Yii::setAlias('@nodeWeb', '@vendor/ratacibernetica/yii2-node-socket/lib/js');
