1ade5bdc6a315f6850d723ac80c69e8ea00e77ce
Ruslan Absalyamov <kazan1811>
2017-08-23 17:26:07 +0300

    remove phpDoc namespace client

--- frontend/views/client/_search.php
+++ frontend/views/client/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\client\ClientSearch */
+/* @var $model app\models\ClientSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
