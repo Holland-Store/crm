396aa8a492b1c627e475480ff0b7dc08cfc51e2b
Ruslan Absalyamov <kazan1811>
2017-08-24 13:13:53 +0300

    create for local server

---
+++ common/config/main-local.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=crm',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCache' => 'cache',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'holland.itkzn@gmail.com',
+                'password' => '*****',
+                'port' => 465,
+                'encryption' => 'ssl',
+                ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'cache' => 'cacheRbac',
+        ],
+        'formatter' => [
+            'timeZone' => 'UTC',
+        ]
+    ],
+];
