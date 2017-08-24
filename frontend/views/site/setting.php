7f52fd121f8fd6d6b3664bcdb469f7334660c354
Ruslan Absalyamov <kazan1811>
2017-08-24 10:56:46 +0300

    tuning setting

--- frontend/views/site/setting.php
+++ frontend/views/site/setting.php
@@ -1,7 +1,26 @@
 <?php
 /* @vat $this \yii\web\View */
+
+//use yii\widgets\DetailView;
+use kartik\detail\DetailView;
+
 /* @var $model \app\models\User */
 ?>
 <a href="http://telegram.me/HollandSotrudbot?start=<?= $model->telegram_token; ?>" target="_blank" class="black-btn btn-lg">
     <i class="fa fa-paper-plane"></i> Подключить
 </a>
+
+<?= DetailView::widget([
+    'model' => $model,
+    'hover' => false,
+    'mode'=>DetailView::MODE_VIEW,
+    'striped' => false,
+    'panel'=>[
+        'heading'=>'Учетная запись для пользователя ' . $model->name,
+        'type'=>DetailView::TYPE_INFO,
+        'headingOptions' => ['template' => '{title}']
+    ],
+    'attributes' => [
+        'email:email'
+    ]
+]) ?>
