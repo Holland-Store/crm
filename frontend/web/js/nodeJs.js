6ca1fe129a4b7c23e750ff7ce7aaf0a2911040c8
Ruslan Absalyamov <kazan1811>
2017-08-29 13:48:32 +0300

    create nodeJs

--- frontend/web/js/nodeJs.js
+++ frontend/web/js/nodeJs.js
@@ -1,44 +1,4 @@
-var socket = new YiiNodeSocket();
 
-socket.debug(true);
-
-socket.onConnect(function () {
-    socket.room('testRoom').join(function (success, numberOfRoomSubscribers) {
-        if(success){
-            console.log(numberOfRoomSubscribers + ' clients in room: '+roomId);
-            // do something
-
-            // bind events
-            this.on('join', function (newMembersCount) {
-                // fire on client join
-            });
-
-            this.on('data', function (data) {
-                // fire when server send frame into this room with 'data' event
-            });
-        } else {
-            // numberOfRoomSubscribers - error message
-            alert(numberOfRoomSubscribers);
-        }
-    })
-});
-
-socket.onDisconnect(function () {
-    // fire when connection close or lost
-});
-
-socket.onConnecting(function () {
-    // fire when the socket is attempting to connect with the server
-});
-
-socket.onReconnect(function () {
-    // fire when successfully reconnected to the server
-});
-
-socket.on('updateBoard', function (data) {
-    // do any action
-});
-
-socket.on('message', function (message) {
-    console.log(message.text);
-});
+var conn = new WebSocket('ws://localhost:8080');
+conn.onmessage = function(e) { console.log(e.data); };
+conn.onopen = function() { conn.send({"idUser":'+yiiConfig["idUser"]+'}); };
