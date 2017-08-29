f4e1b8e4a0e55e4b2517e808e573b8a066e4c4d1
Ruslan Absalyamov <kazan1811>
2017-08-29 13:48:50 +0300

    create file

---
+++ console/controllers/SocketController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace console\controllers;
+
+use console\models\SocketServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use yii\console\Controller;
+
+class SocketController extends Controller
+{
+    public function actionStartSocket($port = 8080)
+    {
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new SocketServer()
+                )
+            ),
+            $port
+        );
+        $server->run();
+    }
+}
