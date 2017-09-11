144c03ed2a57bbdf6ceed595f0db62f83a119cb4
Ruslan Absalyamov <kazan1811>
2017-09-11 15:49:13 +0300

    add table crud

--- frontend/views/personnel/index.php
+++ frontend/views/personnel/index.php
@@ -1,13 +1,43 @@
 <?php
-/* @var $this yii\web\View */
-/* @var $model app\models\Personnel */
-/* @var $count app\models\Personnel */
-?>
-<h1>Сотрудники</h1>
 
-<p>
-    <?php foreach ($model as $sotrud){
-        $n++;
-        echo $n.''.$sotrud->last_name.' '.$sotrud->name.' '.$sotrud->phone.' '.$sotrud->idPosition->name.'<br>';
-    } ?>
-</p>
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\PersonnelSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Сотрудники';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="personnel-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+<?php Pjax::begin(); ?>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'tableOptions' => ['class' => 'table table-bordered'],
+        'columns' => [
+            [
+                'attribute' => 'nameSotrud',
+            ],
+            [
+                'attribute' => 'phone',
+            ],
+            [
+               'attribute' => 'positions',
+                'filter' => \app\models\Position::find()->select(['name', 'id'])->indexBy('id')->column(),
+                'value' => 'positionsAsString',
+            ],
+            [
+                'attribute' => 'job_duties',
+                'value' => function($model){
+                    return $model->job_duties != null ? $model->job_duties : false;
+                }
+            ],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?></div>
