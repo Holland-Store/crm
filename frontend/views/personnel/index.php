ad52f6a4c7612a17f67c129823fbc4894e827e8f
Ruslan Absalyamov <kazan1811>
2017-09-11 17:49:09 +0300

    add Alert widget and transform gridview

--- frontend/views/personnel/index.php
+++ frontend/views/personnel/index.php
@@ -1,43 +1,55 @@
 <?php
 
-use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\widgets\Alert;
 use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\PersonnelSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Сотрудники';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Контакты';
 ?>
 <div class="personnel-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
+    <p>
+        <?php echo Alert::widget([
+            'options' => ['class' => 'infoContact'],
+            'body' => '<b>Внимание!</b> Если у Вас не форс-мажор или особая ситуация, не терпящая отлагательств (срочный крупный заказ; разгневанный клиент; любые контролирующие органы; поломка, которая сильно влияет на процесс работы), просьба звонить коллегам только в их рабочее время. Если все же случилась особая ситуация, то, в первую очередь, просьба обращаться к своему непосредственному руководителю. Если он не отвечает - то его руководителю, и так далее.'
+        ]) ?>
+    </p>
+
 <?php Pjax::begin(); ?>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'tableOptions' => ['class' => 'table table-bordered'],
-        'columns' => [
-            [
-                'attribute' => 'nameSotrud',
-            ],
-            [
-                'attribute' => 'phone',
-            ],
-            [
-               'attribute' => 'positions',
-                'filter' => \app\models\Position::find()->select(['name', 'id'])->indexBy('id')->column(),
-                'value' => 'positionsAsString',
-            ],
-            [
-                'attribute' => 'job_duties',
-                'value' => function($model){
-                    return $model->job_duties != null ? $model->job_duties : false;
-                }
-            ],
-        ],
-    ]); ?>
+<!--    --><?//= GridView::widget([
+//        'dataProvider' => $dataProvider,
+//        'filterModel' => $searchModel,
+//        'tableOptions' => ['class' => 'table table-bordered'],
+//        'showHeader' => false,
+//        'columns' => [
+//            [
+//                'attribute' => 'positions',
+//                'filter' => \app\models\Position::find()->select(['name', 'id'])->indexBy('id')->column(),
+//                'value' => 'positionsAsString',
+//            ],
+//            [
+//                'attribute' => 'nameSotrud',
+//            ],
+//            [
+//                'attribute' => 'phone',
+//            ],
+//            [
+//                'attribute' => 'shedule',
+//                'value' => function($model){
+//                    return $model->shedule != null ? $model->shedule : false;
+//                },
+//            ],
+//            [
+//                'attribute' => 'job_duties',
+//                'value' => function($model){
+//                    return $model->job_duties != null ? $model->job_duties : false;
+//                }
+//            ],
+//        ],
+//    ]); ?>
+
 <?php Pjax::end(); ?></div>
