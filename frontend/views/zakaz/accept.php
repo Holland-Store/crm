c8bb2ab2a318f440f02825151cc8477fb4b3deb9
Ruslan Absalyamov <kazan1811>
2017-09-12 15:35:05 +0300

    add field id_autsors

--- frontend/views/zakaz/accept.php
+++ frontend/views/zakaz/accept.php
@@ -1,8 +1,12 @@
 <?php
+
+use app\models\Partners;
 use app\models\Zakaz;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 /** @var $model app\models\Zakaz */
+/** @var $autsors app\models\Partners */
 ?>
 
 <?php $form = ActiveForm::begin([
@@ -14,6 +18,14 @@ use yii\widgets\ActiveForm;
     Zakaz::STATUS_MASTER => 'Мастер',
     Zakaz::STATUS_AUTSORS => 'Аутсорс',
 ])->label(false)?>
+
+<?= $form->field($model, 'id_autsors')->dropDownList(
+    ArrayHelper::map(Partners::find()->all(), 'id', 'name'),
+    [
+        'prompt' => 'Выберите партнера',
+        'id' => 'autsors'
+    ]
+)->label(false) ?>
 
 <?= Html::submitButton('Да', ['class' => 'action']) ?>
 
