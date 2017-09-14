46042a9f4207cedef9075d0138b75d08bb7bfa8e
Ruslan Absalyamov <kazan1811>
2017-09-14 10:50:02 +0300

    add form for Shifts models

--- frontend/views/site/setting.php
+++ frontend/views/site/setting.php
@@ -1,11 +1,16 @@
 <?php
 /* @vat $this \yii\web\View */
-
-use kartik\detail\DetailView;
-use yii\helpers\ArrayHelper;
-
 /* @var $model \app\models\User */
 /* @var $sotrud \app\models\Shifts */
+/* @var $shifts \app\models\Shifts */
+/** @var array $shifts */
+
+use app\models\Personnel;
+use kartik\detail\DetailView;
+use kartik\form\ActiveForm;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
 ?>
 <a href="http://telegram.me/HollandSotrudbot?start=<?= $model->telegram_token; ?>" target="_blank" class="black-btn btn-lg">
     <i class="fa fa-paper-plane"></i> Подключить
@@ -37,3 +42,27 @@ use yii\helpers\ArrayHelper;
         'personnelAsString',
     ]
 ]) ?>
+
+<?= Html::submitButton('Начать смену', ['class' => 'btn action']); ?>
+<?php if($shifts != null): ?>
+<?= Html::submitButton('Закончить смену', ['class' => 'btn action']); ?>
+<?php endif; ?>
+<?php $form = ActiveForm::begin([
+        'id' => 'form-startShift'
+]); ?>
+
+<?= $form->field($sotrud, 'id_sotrud')->dropDownList(ArrayHelper::map(Personnel::find()->where(['action' => 0])->all(), 'id', 'nameSotrud'),
+    [
+        'prompt' => 'Выберите сотрудника',
+    ])->label(false) ?>
+
+<?php ActiveForm::end(); ?>
+<?php $form = ActiveForm::begin([
+    'id' => 'form-endShift'
+]); ?>
+<?= $form->field($sotrud, 'id_sotrud')->dropDownList(ArrayHelper::map($shifts, 'id', 'nameSotrud'),
+    [
+        'prompt' => 'Выберите сотрудника',
+    ])->label(false) ?>
+
+<?php ActiveForm::end(); ?>
