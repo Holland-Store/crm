1e01b1dd9e9f256df3fc5a222d59bd608ed1e7ef
Ruslan Absalyamov <kazan1811>
2017-09-18 10:39:16 +0300

    add table and  button for each role

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,52 +2,119 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+use yii\bootstrap\Html;
+
+$this->title = 'Главная страница';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <p class="lead">Если Вы ознакомились, то нажмите на "ОК"</p>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p>
+        <?php if (Yii::$app->user->can('admin')): ?>
+            <?= Html::a(Html::encode('ОК'), ['zakaz/admin'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        <?php if (Yii::$app->user->can('shop')): ?>
+            <?= Html::a(Html::encode('ОК'), ['zakaz/shop'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        <?php if (Yii::$app->user->can('disain')): ?>
+            <?= Html::a(Html::encode('ОК'), ['zakaz/disain'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        <?php if (Yii::$app->user->can('master')): ?>
+            <?= Html::a(Html::encode('ОК'), ['zakaz/master'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        <?php if (Yii::$app->user->can('system')): ?>
+            <?= Html::a(Html::encode('ОК'), ['helpdesk/index'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        <?php if (Yii::$app->user->can('zakup')): ?>
+            <?= Html::a(Html::encode('ОК'), ['custom/index'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        <?php if (Yii::$app->user->can('courier')): ?>
+            <?= Html::a(Html::encode('ОК'), ['courier/index'], ['class' => 'btn btn-lg action']) ?>
+        <?php endif; ?>
+        </p>
     </div>
 
-    <div class="body-content">
+    <div class="container">
+        <div></div>
+    </div>
+</div>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+<h2><?= Html::encode('Сроки') ?></h2>
+<div class="container srokIndex">
+    <table>
+        <tr>
+            <td>Оцифровка аудио/фото/видео</td>
+            <td>1 рабочий день</td>
+        </tr>
+        <tr>
+            <td>Значки</td>
+            <td>1 рабочий день</td>
+        </tr>
+        <tr>
+            <td>Печать на кепке</td>
+            <td>1 рабочий день</td>
+        </tr>
+        <tr>
+            <td>Холсты</td>
+            <td>1-2 дня</td>
+        </tr>
+        <tr>
+            <td>Баннеры, печать на ткани, пвх, печать больших наклеек</td>
+            <td>1-2 дня</td>
+        </tr>
+        <tr>
+            <td>Визитки 100шт</td>
+            <td>В течение дня</td>
+        </tr>
+        <tr>
+            <td>Визитки 500шт</td>
+            <td>4 рабочих дней</td>
+        </tr>
+        <tr>
+            <td>Визитки 1000шт</td>
+            <td>3 рабочий день</td>
+        </tr>
+        <tr>
+            <td>Керамика</td>
+            <td>10 рабочих дней</td>
+        </tr>
+        <tr>
+            <td>Листовки чб и цветные</td>
+            <td>2-3 рабочих дня</td>
+        </tr>
+        <tr>
+            <td>Евробуклеты</td>
+            <td>4-5 рабочих дней</td>
+        </tr>
+        <tr>
+            <td>Проявка</td>
+            <td>2-3 рабочих дня</td>
+        </tr>
+        <tr>
+            <td>Нашивки</td>
+            <td>Около 2-х недель</td>
+        </tr>
+        <tr>
+            <td>Фото книги</td>
+            <td>1 неделя</td>
+        </tr>
+        <tr>
+            <td>Пластиковые карты с чипированием, штрих код и т.д</td>
+            <td>2-7 дней</td>
+        </tr>
+        <tr>
+            <td>Бейджи пластиковые со сменным именем</td>
+            <td>10 дней</td>
+        </tr>
+        <tr>
+            <td>Оцветнение фото</td>
+            <td>2 дня</td>
+        </tr>
+    </table>
+    <div>
+        Все сроки указаны с готовым макетом, либо после согласования макета.<br>
+        Также мы изготавливаем ручки, флешки, зажигалки, телефоны, ноутбуки, ежедневники с печатью и гравировкой. –сроки 1-2 дня
     </div>
 </div>
