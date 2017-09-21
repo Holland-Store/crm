db98ba005490f45d4631a6525f96457e4adde122
Ruslan Absalyamov <kazan1811>
2017-09-21 13:54:11 +0300

    remove title

--- frontend/views/courier/create.php
+++ frontend/views/courier/create.php
@@ -6,13 +6,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Courier */
 
-$this->title = 'Create Courier';
-$this->params['breadcrumbs'][] = ['label' => 'Couriers', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Создать доставку';
+//$this->params['breadcrumbs'][] = ['label' => 'Couriers', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="courier-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
