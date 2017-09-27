ce814beed32dc863aa8d8c9ab33e6324b396e682
Ruslan Absalyamov <kazan1811>
2017-09-27 15:15:20 +0300

    remove extra and translate button

--- frontend/views/todoist/_search.php
+++ frontend/views/todoist/_search.php
@@ -15,17 +15,13 @@ use yii\widgets\ActiveForm;
         'method' => 'get',
     ]); ?>
 
-    <?= $form->field($model, 'id') ?>
-
-    <?= $form->field($model, 'srok') ?>
-
     <?= $form->field($model, 'id_zakaz') ?>
 
     <?= $form->field($model, 'comment') ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+        <?= Html::submitButton('Поиск', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Сбросить', ['class' => 'btn btn-default']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
