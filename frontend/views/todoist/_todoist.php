a4c3acdd412a5c0a6117a12b8fe2ba34f4f3ecba
Ruslan Absalyamov <kazan1811>
2017-09-27 15:17:19 +0300

    create file todoist

---
+++ frontend/views/todoist/_todoist.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Todoist */
+/* @var $comment app\models\Comment */
+
+
+?>
+
+<div class="todoist-detail">
+    <div>
+        <?= $comment->comment ?>
+    </div>
+</div>
