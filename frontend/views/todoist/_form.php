4e112fc2c5abbfd6053cb4c62475cc564adc2c07
Ruslan Absalyamov <kazan1811>
2017-09-29 10:33:16 +0300

    add fild file widget FileInput

--- frontend/views/todoist/_form.php
+++ frontend/views/todoist/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\models\User;
+use kartik\widgets\FileInput;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
@@ -39,9 +40,58 @@ use kartik\widgets\DatePicker;
 
     <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
 
+    <div class="col-lg-3">
+        <?= $form->field($model, 'file')->widget(FileInput::className(), [
+            'language' => 'ru',
+            'options' => ['multiple' => false],
+            'pluginOptions' => [
+                'theme' => 'explorer',
+                'showCaption' => false,
+                'showRemove' => false,
+                'showUpload' => false,
+                'showPreview' => true,
+                'browseClass' => 'action fileInput',
+                'browseLabel' =>  'Загрузить файл',
+                'previewFileType' => 'any',
+                'maxFileCount' => 1,
+                'maxFileSize' => 25600,
+                'preferIconicPreview' => true,
+                'previewFileIconSettings' => ([
+                    'doc' => '<i class="fa fa-file-word-o text-orange"></i>',
+                    'docx' => '<i class="fa fa-file-word-o text-orange"></i>',
+                    'xls' => '<i class="fa fa-file-excel-o text-orange"></i>',
+                    'xlsx' => '<i class="fa fa-file-excel-o text-orange"></i>',
+                    'ppt' => '<i class="fa fa-file-powerpoint-o text-orange"></i>',
+                    'pptx' => '<i class="fa fa-file-powerpoint-o text-orange"></i>',
+                    'pdf' => '<i class="fa fa-file-pdf-o text-orange"></i>',
+                    'zip' => '<i class="fa fa-file-archive-o text-orange"></i>',
+                    'rar' => '<i class="fa fa-file-archive-o text-orange"></i>',
+                    'txt' => '<i class="fa fa-file-text-o text-orange"></i>',
+                    'jpg' => '<i class="fa fa-file-photo-o text-orange"></i>',
+                    'jpeg' => '<i class="fa fa-file-photo-o text-orange"></i>',
+                    'png' => '<i class="fa fa-file-photo-o text-orange"></i>',
+                    'gif' => '<i class="fa fa-file-photo-o text-orange"></i>',
+                    'cdr' => '<i class="fa fa-file-photo-o text-orange"></i>',
+                ]),
+                'layoutTemplates' => [
+                    'preview' => '<div class="file-preview {class}">
+                               <div class="{dropClass}">
+                               <div class="file-preview-thumbnails">
+                                </div>
+                                <div class="clearfix"></div>
+                                <div class="file-preview-status text-center text-success"></div>
+                                <div class="kv-fileinput-error"></div>
+                                </div>
+                                </div>',
+                    'actionDrag' => '<span class="file-drag-handle {dragClass}" title="{dragTitle}">{dragIcon}</span>',
+                ],
+            ]
+        ]) ?>
+    </div>
+
     <?= $form->field($model, 'id_sotrud_put')->hiddenInput(['value' => Yii::$app->user->id])->label(false) ?>
 
-    <div class="form-group">
+    <div class="form-group col-lg-12">
         <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Редактировать', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
