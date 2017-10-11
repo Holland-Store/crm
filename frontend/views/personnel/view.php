d8fa1b8460856dc2683b872e0e9252a11af312c8
Ruslan Absalyamov <kazan1811>
2017-10-11 16:45:42 +0300

    add sum salary and find, bonus

--- frontend/views/personnel/view.php
+++ frontend/views/personnel/view.php
@@ -1,7 +1,9 @@
 <?php
 
+use app\models\Personnel;
 use yii\bootstrap\Modal;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Shifts */
@@ -9,6 +11,7 @@ use yii\helpers\Html;
 /* @var $position app\models\Position */
 /* @var $sumShifts app\models\Shifts */
 /* @var $sumFinancy app\models\Financy */
+/* @var $sumWage app\models\Financy */
 /* @var $financy app\models\Financy */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
@@ -18,7 +21,7 @@ $this->title = $modelPersonnel->nameSotrud;
 <div class="col-lg-12">
     <?=Html::submitButton('Назначить', [
             'class' => 'btn btn-success financy',
-            'value' => \yii\helpers\Url::to(['financy/charge', 'id' => $modelPersonnel->id])
+            'value' => Url::to(['financy/charge', 'id' => $modelPersonnel->id])
         ]) ?>
     <?php Modal::begin([
         'id' => 'financeModel'
@@ -26,8 +29,8 @@ $this->title = $modelPersonnel->nameSotrud;
     echo '<div class="modalContent"></div>';
     Modal::end()?>
 </div>
-<div class="col-lg-4">
-    <h4><?php echo 'График работы '.$modelPersonnel->sheduleName ?></h4>
+<div class="col-lg-3">
+    <h4><?= Html::encode('График работы '.$modelPersonnel->sheduleName) ?></h4>
     <h3><?=Html::encode('Смены') ?></h3>
         <?php if (!$model){
             echo 'Ничего не найдена';
@@ -39,12 +42,12 @@ $this->title = $modelPersonnel->nameSotrud;
             </tr>';
             foreach ($model as $shifts){
                 echo '<tr><td style="padding: 8px">'.Yii::$app->formatter->asDate($shifts->start).'</td>
-                <td style="padding: 8px">'.(date('H', $shifts->number)*1).'</td></tr>';
+                <td style="padding: 8px">'.intval($shifts->number/60).'</td></tr>';
             }
             /** @var string $sumShifts */
             echo '<tr>
             <th>Итого</th>
-            <th>'.(date('H', $sumShifts)*1).' часов</th>
+            <th>'.intval($sumShifts/60).' часов</th>
         </tr>
     </table>';
     }?>
@@ -62,7 +65,7 @@ $this->title = $modelPersonnel->nameSotrud;
                 <th>Штраф/Премия</th>
 </tr>';
         foreach ($financy as $fin) {
-            echo '<tr><td style="padding: 8px">'.$fin->date.'</td>
+            echo '<tr><td style="padding: 8px">'.Yii::$app->formatter->asDate($fin->date).'</td>
             <td style="padding: 8px">'.$fin->sum.' рублей</td>
             <td style="padding: 8px">'.$fin->comment.'</td>
             <td style="padding: 8px">'.$fin->categoryName.'</td></tr>';
@@ -70,20 +73,32 @@ $this->title = $modelPersonnel->nameSotrud;
         /** @var string $sumFinancy */
         echo '<tr>
             <th>Итого</th>
-            <th>'.$sumFinancy.' рублей</th>
+            <th>'.$sumWage.' рублей</th>
         </tr>';
-'</table>';
+       echo '</table>';
     }?>
 </div>
 <div class="col-lg-3">
     <h3><?= Html::encode('Зарплата') ?></h3>
     <div>Оклад:
         <?php foreach ($modelPersonnel->positions as $key => $value){
-            echo number_format($value->salary, 0, ',', ' ').' рублей<br>';
+            $salary = $value->salary;
+            echo number_format($salary, 0, ',', ' ').' рублей<br>';
         } ?>
     </div>
     <div>Премия: <?php echo $modelPersonnel->bonus.' рублей' ?></div>
-    <div>Итого: <?php echo 55.56*date('G,i', $sumShifts+16400) ?></div>
-</div>
-<div class="col-lg-12">
+    <div>Итого: <?php if ($modelPersonnel->shedule == Personnel::SHEDULE_DOUBLE){
+            $shedule = 15;
+            $timeWork = 12;
+        } else {
+            $shedule = 20;
+            $timeWork = 8;
+        }
+        $bonus = $modelPersonnel->bonus;
+        $salaryPrize = $salary+$bonus;
+        $sumTime = $salaryPrize/$shedule/$timeWork/60;
+        $wage = ($sumShifts*$sumTime).'<br>';
+        echo '<b>'.number_format($wage,2, ',', ' ').'</b> рублей<br>';
+        echo 'Штрафами и премиями '.number_format($wage-$sumWage, 2, ',', ' ').' рублей'; ?>
+    </div>
 </div>
