5819cac3188643261dc4cd4b27cb68ee7cab9ede
Ruslan Absalyamov <kazan1811>
2017-10-19 13:52:45 +0300

    added new action Helpdesk

--- frontend/controllers/CommentController.php
+++ frontend/controllers/CommentController.php
@@ -3,7 +3,9 @@
 namespace frontend\controllers;
 
 use app\models\Comment;
+use app\models\Helpdesk;
 use app\models\Todoist;
+use app\models\User;
 use frontend\models\Telegram;
 use Yii;
 use yii\filters\AccessControl;
@@ -51,15 +53,37 @@ class CommentController extends Controller
                 print_r($commentForm->getErrors());
             } else {
                 if ($todoist->id_sotrud_put != $user){
-                    $telegram->message($todoist->id_sotrud_put, 'Задачу '.$commentForm->id_todoist.' прокомментировали '.$commentForm->comment);
+                    $telegram->message($todoist->id_sotrud_put, 'Задачу '.$commentForm->idTodoist->comment.' прокомментировали '.$commentForm->comment);
                 } elseif($todoist->id_user != $user){
-                    $telegram->message($todoist->id_user, 'Задачу '.$commentForm->id_todoist.' прокомментировали '.$commentForm->comment);
+                    $telegram->message($todoist->id_user, 'Задачу '.$commentForm->idTodoist->comment.' прокомментировали '.$commentForm->comment);
                 }
                 if (Yii::$app->user->can('admin')){
                     return $this->redirect(['todoist/index']);
                 } else {
                     return $this->redirect(['todoist/shop']);
                 }
+            };
+        }
+    }
+
+    public function actionHelpdesk($id)
+    {
+        $commentForm = new Comment();
+        $telegram = new Telegram();
+        $helpdesk = Helpdesk::findOne($id);
+
+        if ($commentForm->load(Yii::$app->request->post())){
+            $commentForm->id_helpdesk = $id;
+            $commentForm->id_user = Yii::$app->user->id;
+            if (!$commentForm->save()){
+                print_r($commentForm->getErrors());
+            } else {
+                if(Yii::$app->user->id != User::USER_SYSTEM){
+                    $telegram->message($helpdesk->id_user, 'Поломку '.$commentForm->idHelpdesk->commetnt.' прокомментировали '.$commentForm->comment);
+                } else {
+                    $telegram->message(User::USER_SYSTEM, 'Поломку '.$commentForm->idHelpdesk->commetnt.' прокомментировали '.$commentForm->comment);
+                }
+                return $this->redirect(['helpdesk/index']);
             };
         }
     }
