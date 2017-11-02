ab558a3ad6f335fb568cfe14a9c88c9e13ed67ba
Ruslan Absalyamov <kazan1811>
2017-11-02 09:25:58 +0300

    changed js file for yandexMap

--- frontend/components/YandexMap.php
+++ frontend/components/YandexMap.php
@@ -12,7 +12,7 @@ use yii\base\Widget;
 
 class YandexMap extends Widget
 {
-    public $data = null;
+    public $index;
     public function init()
     {
         parent::init();
@@ -20,7 +20,43 @@ class YandexMap extends Widget
 
     public function run()
     {
-        $this->view->registerJsFile('https://api-maps.yandex.ru/2.1/?lang=ru_RU', ['type' => 'text/javascript']);
-        $this->view->registerJsFile('@web/js/yandexMap.js');
+        $this->view->registerJsFile('https://api-maps.yandex.ru/2.1/?lang=ru_RU');
+        if ($this->index == 'courier'){
+            $this->view->registerJsFile('@web/js/yandexMap.js');
+        } else {
+            $this->view->registerJs('
+            let searchControl,
+            suggestView;
+        
+        ymaps.ready(init);
+        
+        function init(){
+            suggestView = new ymaps.SuggestView("toMap");
+            searchControl = new ymaps.control.SearchControl({
+                options: {
+                    float: "left",
+                    floatIndex: 100,
+                    noPlacemark: true
+                }
+            });
+        
+            $("#toMap").on("change", function () {
+                let value = $(this).val();
+                searchControl.search(value).then(function () {
+                    let geoOjectsArray = searchControl.getResultsArray();
+                    if(geoOjectsArray.length){
+                        let city = geoOjectsArray[0].properties.get("description");
+                        let cityArr = city.split(",");
+                        let coordinat = geoOjectsArray[0].geometry.getCoordinates();
+                        let name = geoOjectsArray[0].properties.get("name");
+                        $("#toInput").val(coordinat);
+                        $("#toName").val(name);
+                        $("#toCity").val(cityArr[2])
+                    }
+                });
+            });
+        }
+            ');
+        }
     }
 }
