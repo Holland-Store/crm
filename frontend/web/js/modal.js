b6ef772057ce481393d36a411276391636285890
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-10 09:50:18 +0300

    added preventDefault for browsers

--- frontend/web/js/modal.js
+++ frontend/web/js/modal.js
@@ -6,19 +6,20 @@ $(document).ready(function() {
     modalView('.draft', '#draftModal');
     modalView('.modalShipping-button', '#modalShipping');
     modalView('.financy', '#financeModel');
-    modalView('.draft', '#draftModal');
     bodyModalView('.createClient', '#modalCreateClient', '.modalContentClient');
     bodyModalView('.declinedTodoist', '#modalDeclinedTodoist', '.modalContent');
 
     function modalView(button, modal) {
-        $(button).click(function () {
+        $(button).click(function (e) {
+            e.preventDefault();
             $(modal).modal('show')
                 .find('.modalContent')
                 .load($(this).attr('value'));
         });
     }
     function bodyModalView(button, modal, content){
-        $('body').on('click', button, function(){
+        $('body').on('click', button, function(e){
+            e.preventDefault();
             $(modal).modal('show')
                 .find(content)
                 .load($(this).attr('value'))
