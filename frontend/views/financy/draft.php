9e536f4d27fdd823f7325a6c57639a4d56041b5e
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-10 09:48:01 +0300

    remove action and added id  draftForm

--- frontend/views/financy/draft.php
+++ frontend/views/financy/draft.php
@@ -8,7 +8,7 @@ use yii\widgets\MaskedInput;
 /** @var $financy app\models\Financy */
 
 $form = ActiveForm::begin([
-        'action' => ['zakaz/draft', 'id' => $model->id_zakaz],
+        'id' => 'draftForm',
     ]); ?>
 
 <div class="draftSum">
