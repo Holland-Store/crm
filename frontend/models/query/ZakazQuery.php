19eec52711cc17e482891f92dfb7ca82f295ebe8
Ruslan Absalyamov <absaruslan9@yandex.ru>
2017-11-10 09:46:22 +0300

    prefics sql request

--- frontend/models/query/ZakazQuery.php
+++ frontend/models/query/ZakazQuery.php
@@ -15,15 +15,15 @@ class ZakazQuery extends ActiveQuery
 {
     public function masterGridView()
     {
-        return $this->andWhere(['status' => [
+        return $this->andWhere(['zakaz.status' => [
             Zakaz::STATUS_MASTER,
             Zakaz::STATUS_DECLINED_MASTER
-        ], 'action' => 1]);
+        ], 'zakaz.action' => 1]);
     }
 
     public function admin()
     {
-        return $this->andWhere(['status' => [
+        return $this->andWhere(['zakaz.status' => [
             Zakaz::STATUS_DISAIN,
             Zakaz::STATUS_MASTER,
             Zakaz::STATUS_AUTSORS,
@@ -31,18 +31,18 @@ class ZakazQuery extends ActiveQuery
             Zakaz::STATUS_SUC_DISAIN,
             Zakaz::STATUS_DECLINED_DISAIN,
             Zakaz::STATUS_DECLINED_MASTER
-        ], 'action' => 1]);
+        ], 'zakaz.action' => 1]);
     }
 
     public function masterAgreed()
     {
-        return $this->andWhere(['status' => Zakaz::STATUS_SUC_MASTER, 'action' => 1]);
+        return $this->andWhere(['zakaz.status' => Zakaz::STATUS_SUC_MASTER, 'zakaz.action' => 1]);
     }
 
     public function disainerGridView()
     {
         return $this->andWhere([
-        'status' => [
+        'zakaz.status' => [
             Zakaz::STATUS_DISAIN,
             Zakaz::STATUS_DECLINED_DISAIN
         ],
@@ -51,25 +51,25 @@ class ZakazQuery extends ActiveQuery
             Zakaz::STATUS_DISAINER_WORK,
             Zakaz::STATUS_DISAINER_DECLINED
         ],
-        'action' => 1]);
+        'zakaz.action' => 1]);
     }
 
     public function disainAgreed()
     {
         return $this->andWhere([
-                'status' => Zakaz::STATUS_DISAIN,
+                'zakaz.status' => Zakaz::STATUS_DISAIN,
                 'statusDisain' => Zakaz::STATUS_DISAINER_SOGLAS,
-                'action' => 1
+                'zakaz.action' => 1
             ])
-            ->orWhere(['status' => Zakaz::STATUS_SUC_DISAIN, 'action' => 1]);
+            ->orWhere(['zakaz.status' => Zakaz::STATUS_SUC_DISAIN, 'zakaz.action' => 1]);
     }
 
     public function shopWorkGridView()
     {
         return $this->andWhere([
             'id_sotrud' => Yii::$app->user->id,
-            'action' => 1,
-            'status' => [
+            'zakaz.action' => 1,
+            'zakaz.status' => [
                 Zakaz::STATUS_DISAIN,
                 Zakaz::STATUS_MASTER,
                 Zakaz::STATUS_AUTSORS,
@@ -85,28 +85,28 @@ class ZakazQuery extends ActiveQuery
 
     public function shopExecute()
     {
-        return $this->andWhere(['id_shop' => Yii::$app->user->id, 'action' => 1, 'status' => Zakaz::STATUS_EXECUTE]);
+        return $this->andWhere(['id_shop' => Yii::$app->user->id, 'zakaz.action' => 1, 'zakaz.status' => Zakaz::STATUS_EXECUTE]);
     }
 
     public function adminWork()
     {
-        return $this->andWhere(['status' => [
+        return $this->andWhere(['zakaz.status' => [
             Zakaz::STATUS_NEW,
             Zakaz::STATUS_ADOPTED,
             Zakaz::STATUS_REJECT
-        ], 'action' => 1]);
+        ], 'zakaz.action' => 1]);
     }
 
     public function adminFulfiled()
     {
-        return $this->andWhere(['status' => Zakaz::STATUS_EXECUTE, 'action' => 1]);
+        return $this->andWhere(['zakaz.status' => Zakaz::STATUS_EXECUTE, 'zakaz.action' => 1]);
     }
 
     public function managerGridView()
     {
         return $this->andWhere(['<', 'srok', date('Y-m-d H:i:s')])
             ->andWhere(['>', 'oplata', 1000])
-            ->andWhere(['action' => 1]);
+            ->andWhere(['zakaz.action' => 1]);
     }
 
     /**
