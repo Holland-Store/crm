76422175871d55a92c751c6ac0e3df65047c84a2
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-04-03 17:24:21 +0300

    added modal for create notice. changed detailview widget, visible shipping. Changed comment

--- frontend/views/zakaz/view.php
+++ frontend/views/zakaz/view.php
@@ -2,9 +2,10 @@
 
 use kartik\form\ActiveForm;
 use kartik\grid\GridView;
-use yii\helpers\ArrayHelper;
+use yii\bootstrap\Modal;
 use yii\helpers\Html;
 use kartik\detail\DetailView;
+use yii\helpers\Url;
 use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
@@ -17,8 +18,6 @@ use yii\widgets\Pjax;
 $this->title = $model->prefics;
 ?>
     <div class="zakaz-view">
-        <?php Pjax::begin(); ?>
-
         <div class="col-xs-6">
         <h4>Информация о заказе</h4>
         <?= DetailView::widget([
@@ -66,16 +65,21 @@ $this->title = $model->prefics;
                 'value' => Yii::$app->formatter->asDatetime($model->data),
                 'visible' => Yii::$app->user->can('seeAdop'),
             ],
-            'information',
+            [
+                'attribute' => 'information',
+                'format' => 'raw',
+                'valueColOptions' => ['class' => 'zakaz-information'],
+                'value' => $notice == null ? $model->information.'<span class="glyphicon glyphicon-plus addNotice" data-toggle="tooltip" title="Добавить примечание" data-href="'.Url::to(["notify/create", 'id' => $model->id_zakaz]).'"></span>' : $notice[0]->comment.' (Редактирован '.Yii::$app->formatter->asDatetime($notice[0]->created_at).')<span class="glyphicon glyphicon-plus addNotice" data-toggle="tooltip" title="Добавить примечание" data-href="'.Url::to(["notify/create", 'id' => $model->id_zakaz]).'"></span>',
+            ],
             [
                 'attribute' => 'img',
                 'format' => 'raw',
-                'value' => $model->img == null ? null : Html::a($model->img, '@web/'.$model->img, ['download' => true, 'data-pjax' => 0])
+                'value' => $model->img == null ? null : Html::a($model->img, '@web/'.$model->img, ['download' => true])
             ],
             [
                 'attribute' => 'maket',
                 'format' => 'raw',
-                'value' => $model->maket == null ? null : Html::a($model->maket, '@web/'.$model->maket, ['download' => true, 'data-pjax' => 0]),
+                'value' => $model->maket == null ? null : Html::a($model->maket, '@web/'.$model->maket, ['download' => true]),
                 'visible' => $model->maket != null
             ],
             [
@@ -123,6 +127,7 @@ $this->title = $model->prefics;
                 }
             } ?>
         </div>
+            <?php if (Yii::$app->user->can('admin')) :?>
         <div class="col-lg-5">
             <h4>Информация  доставках</h4>
             <?= GridView::widget([
@@ -130,7 +135,9 @@ $this->title = $model->prefics;
                 'headerRowOptions' => ['class' => 'headerTable'],
                 'striped' => false,
                 'condensed' => true,
+                'containerOptions' => ['class' => 'table-responsive zakaz-view_shipping'],
                 'pjax' => true,
+
                 'columns' => [
                     [
                         'attribute' => 'date',
@@ -150,22 +157,59 @@ $this->title = $model->prefics;
                 ]
             ]) ?>
         </div>
+        <?php endif; ?>
         <div class="col-lg-6">
-            <h4><?= Html::encode('Комментарии') ?></h4>
             <?php Pjax::begin([
                 'id' => 'commentPjax'
             ]) ?>
-            <?php if ($comment == null){
-                echo 'Комментариев пока нет';
-            } else {
-                foreach ($comment as $key=>$com){
-                    echo '<b>'.Yii::$app->formatter->asDate($key, 'php:j M Y').'</b><br>';
-                    foreach ($com as $value=>$name){
-                        echo Yii::$app->formatter->asTime(ArrayHelper::getValue($name, 'time'), 'php:H:i').' '.ArrayHelper::getValue($name, 'comment').' '.ArrayHelper::getValue($name, 'idUser.name').'<br>';
-                    }
-                }
-            } ?>
+            <?php if ($notice != null): ?>
+                <div class="notice-block"><?= '<span class="notice-sotrud">'.$notice[0]->user->name.'</span> '.$notice[0]->comment.' <span class="notice-action pull-right">'.Yii::$app->formatter->asDatetime($notice[0]->created_at).'</span>' ?></div>
+                <?php echo count($notice[0]->comments) >= 6 ? '<div class="zakaz-view_comment">' : '<div>' ?>
+                <?php foreach ($comment as $item): ?>
+                    <?php if ($item['notice_id'] != null): ?>
+                        <?php if ($notice[0]->id == $item['notice_id']): ?>
+                        <div><span class="notice-sotrud"><?= $item['idUser']['name'] ?></span><?= $item['comment'] ?><span class="notice-action pull-right"><?= Yii::$app->formatter->asDatetime($item['date']) ?></span></div>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+                </div>
+                <?php foreach ($notice as $key => $value): ?>
+                    <?php if ($key != 0): ?>
+                    <div class="notice-block"><?= '<span class="notice-sotrud">'.$value->user->name.'</span> '.$value->comment.' <span class="notice-action pull-right">'.Yii::$app->formatter->asDatetime($value->created_at).'</span>' ?></div>
+                        <?php echo count($notice->comments) >= 6 ? '<div class="zakaz-view_comment">' : '<div>' ?>
+                        <?php foreach ($comment as $item): ?>
+                        <?php if ($item['notice_id'] != null): ?>
+                            <?php if ($notice[0]->id != $item['notice_id']): ?>
+                            <div><span class="notice-sotrud"><?= $item['idUser']['name'] ?></span><?= $item['comment'] ?><span class="notice-action pull-right"><?= Yii::$app->formatter->asDatetime($item['date']) ?></span></div>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+                </div>
+                <div class="notice-block"><?= '<span class="notice-sotrud">'.$model->idSotrud->name.'</span>'.$model->information.' <span class="notice-action pull-right">Основной</span>' ?></div>
+                <?php echo count($model->comments) >= 6 ? '<div class="zakaz-view_comment">' : '<div>' ?>
+                <?php foreach ($comment as $item): ?>
+                    <?php if ($item['notice_id'] == null): ?>
+                        <div><span class="notice-sotrud"><?= $item['idUser']['name'] ?></span><?= $item['comment'] ?><span class="notice-action pull-right"><?= Yii::$app->formatter->asDatetime($item['date']) ?></span></div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
             <?php Pjax::end() ?>
+        </div>
+        <div class="col-lg-6">
+            <h4><?= Html::encode('Комментарии') ?></h4>
+<!--            --><?php //if ($comment == null){
+//                echo 'Комментариев пока нет';
+//            } else {
+//                foreach ($comment as $key=>$com){
+//                    echo '<b>'.Yii::$app->formatter->asDate($key, 'php:j M Y').'</b><br>';
+//                    foreach ($com as $value=>$name){
+//                        echo Yii::$app->formatter->asTime(ArrayHelper::getValue($name, 'time'), 'php:H:i').' '.ArrayHelper::getValue($name, 'comment').' '.ArrayHelper::getValue($name, 'idUser.name').'<br>';
+//                    }
+//                }
+//            } ?>
             <?php $form = ActiveForm::begin([
                     'id' => 'formComment',
                     'action' => ['comment/zakaz'],
@@ -176,8 +220,20 @@ $this->title = $model->prefics;
             <?= Html::submitButton('Отправить', ['class' => 'btn action']) ?>
             <?php ActiveForm::end() ?>
         </div>
-        <?php Pjax::end(); ?>
-    </div>
+</div>
+<?php Modal::begin([
+    'id' => 'create-modal_notify'
+]);
+
+echo '<div class="create-notify">';
+echo Html::beginForm(['notice/create', 'id' => $model->id_zakaz], 'post', ['id' => 'form-create_notify']);
+echo Html::textarea('comment', null, ['class' => 'form-control', 'placeholder' => 'Введите примечание']);
+echo Html::submitButton('Создать', ['class' => 'btn btn-primary']);
+echo Html::endForm();
+echo '</div>';
+echo '</div>';
+
+Modal::end()?>
 <?php $script = <<<JS
     $('#formComment').on('beforeSubmit', function(e) {
       let form = $(this);
@@ -186,7 +242,7 @@ $this->title = $model->prefics;
           form.serialize()
       )
       .done(function(result) {
-        if (result === true){
+        if (result == true){
             $.pjax.reload({container: '#commentPjax'});
             $('#formComment').trigger('reset');
         } else {
@@ -196,6 +252,6 @@ $this->title = $model->prefics;
            console.log('server error');
       });
       return false
-    })
+    });
 JS;
 $this->registerJs($script)?>
