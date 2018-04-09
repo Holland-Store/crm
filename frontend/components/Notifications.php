77c0e5484fee51f21af3586cb056ce5215f6c6c9
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-04-09 11:45:48 +0300

    Changed notification and refactor code

--- frontend/components/Notifications.php
+++ frontend/components/Notifications.php
@@ -16,56 +16,20 @@ class Notifications extends Widget
 {
     public function run()
     {
-        return $this->renderItems();
-    }
-
-    /**
-     * Creates a window for the notification
-     * @return string
-     */
-    public function renderItems()
-    {
-        $notifModel = Notification::find();
-        $notifications = $notifModel->where(['id_user' => Yii::$app->user->id, 'active' => true])->all();
-        $this->renderCount();
-        foreach($notifications as $notification){
-            $date = date('Y-m-d H:i:s', time());
-            if ($notification->category == 0) {
-                $notif = '<span class="glyphicon glyphicon-road"></span> '.$notification->name.'<br>';
-            } elseif ($notification->category == 1) {
-                $notif = '<span class="glyphicon glyphicon-ok"></span> '.$notification->name.'<br>';
-            } elseif ($notification->category == 2) {
-                $notif = '<span class="glyphicon glyphicon-file"></span> '.$notification->name.'<br>';
-            } elseif ($notification->category == 4 && $notification->srok <= $date){
-                $notif = 'Напоминание о заказе №'.$notification->id_zakaz.' '.$notification->srok;
-            } elseif ($notification->category == 4 && $notification->srok >= $date){
-                $notif = '';
-            }
-            /** @var string $notif */
-            echo Html::a($notif.'<br>', ['notification/notification', 'id' => $notification->id_zakaz], ['id' => $notification->id_zakaz, 'class' => 'zakaz', 'data-key' => $notification->id_zakaz]);
-        };
-        $formNotif = '<div class="notification-container hidden" id="notification-container">
-                    <div class="notification-content">'.$notifications.'</div>
-                <div class="notification-footer">'.Html::a('Прочитать все напоминание', ['notification/index']).'</div>
+        if (!Yii::$app->user->isGuest){
+            $notifications = Notification::find()->where(['id_user' => Yii::$app->user->id, 'active' => 1])->all();
+            echo '<div class="notification">';
+                echo $notifications == null ? '<div class="notification-icon">' : '<div class="notification-icon newNotification">';
+                    echo '<span class="glyphicon glyphicon-bell"></span>
+                </div>
+                <div class="notification-container hidden">
+                <div class="notification-container_message">';
+                foreach ($notifications as $notification){
+                    echo '<div>'.Html::a($notification->name, ['notification/read-notice', 'id' => $notification->id_zakaz], ['class' => 'notification-content']).'</div>';
+                }
+                echo '</div><div class="notification-all"><span>'.Html::a('Показать все', ['notification/index']).'</span></div>';
+                echo '</div>
             </div>';
-        return $formNotif;
-    }
-
-    /**
-     * Notification count row
-     * @return string
-     */
-    public function renderCount()
-    {
-        $notifModel = Notification::find();
-        $notification = $notifModel->where(['id_user' => Yii::$app->user->id, 'active' => true])->count();
-        if ($notification > 50) {
-            $notifications = '50+';
-        } elseif ($notification < 1) {
-            $notifications = '';
-        } else {
-            $notifications = $notification;
         }
-        return '<span class="glyphicon glyphicon-bell" style="font-size:21px"></span><span class="badge pull-right">'.$notifications.'</span>';
     }
 }
