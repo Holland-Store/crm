da5a3d17dcdb73af2a3702d35441ee8903995ca2
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-04-09 11:51:18 +0300

    in action create added notification

--- frontend/controllers/NoticeController.php
+++ frontend/controllers/NoticeController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use app\models\Notice;
+use app\models\Notification;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -28,12 +29,15 @@ class NoticeController extends Controller
     public function actionCreate($id)
     {
         $request = Yii::$app->request;
+        $notification = new Notification();
         if(Yii::$app->request->post()){
             $model = new Notice();
             $model->comment = $request->post('comment');
             $model->user_id = Yii::$app->user->id;
             $model->order_id = $id;
             $model->save();
+            $notification->getCreateNotice(Yii::$app->user->id, $id);
+            $notification->save();
             return $this->redirect(['zakaz/view', 'id' => $id]);
         }
         return $this->renderAjax('create');
