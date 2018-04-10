e37e805ee478862c487900f274fe528678f089cc
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-04-10 11:23:03 +0300

    added url for local and moved all notification and new notification

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -5,12 +5,31 @@ $(document).ready(function(){
 // 	setInterval(function(){
 // 		$.pjax.reload('#pjax-container')
 // 	}, 100000);
+        let url = window.location.origin;
         let urlSite = window.location.origin+'/frontend/web';
         changeStatus('.trNew', urlSite+'/zakaz/adopted?id=');
         changeStatus('.trNewMaster', urlSite+'/zakaz/adopmaster?id=');
         changeStatus('.trNewDisain', urlSite+'/zakaz/adopdisain?id=');
         addClassForm('.startShift', '#form-startShift', '.form-shiftStart');
         addClassForm('.endShift', '#form-endShift', '.form-shiftEnd');
+
+        $('body').on('click', '#notification-new', function (e) {
+            e.preventDefault();
+            $.get(`${url}/notification/index?new=true`)
+                .done(res => {
+                    notificationView(res, 'Показать все уведомление', 'notification-all');
+                })
+                .fail(err => console.log(err.responseText))
+        });
+        $('body').on('click', '#notification-all', function (e) {
+            e.preventDefault();
+            let count = $(this).parent().data('count');
+            $.get(`${url}/notification/index`)
+                .done(res => {
+                    notificationView(res, `Новый уведомление: ${count}`, `notification-new`)
+                })
+                .fail(err => console.error(err.responseText))
+        })
 
         $( 'body' ).on( 'click', '.commentButton', function() {
             $( ".CommentForm" ).toggleClass( "CommentForm-visible" );
@@ -64,6 +83,18 @@ $(document).ready(function(){
                 .done(() => console.log('Статус успешно изменен'))
                 .fail(err => console.error(err))
         });
+    }
+    function notificationView(res, message, idName){
+        res = JSON.parse(res);
+        console.log(res);
+        let style = '';
+        let view = res.map(item => {
+            if (idName === 'notification-new'){
+                style = item.active === '1' ? 'font-weight:bold' : '';
+            }
+            return `<p><a href="${url}/notification/open-notification?id=${item.id}" style="${style}">${item.name}</a></p>`
+        }).join(' ');
+        $('.notification-info').html(`<a id=${idName}>${message}</a>${view}`);
     }
     $(function () {
         $("[data-toggle = 'tooltip']").tooltip();
