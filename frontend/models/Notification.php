b2686ec734c1c6de272697f78ac3f4e24e3639c2
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-04-13 17:46:00 +0300

    add todoinst and add public function getByIdNotificationTodoList adn change  public function getByIdNotification

--- frontend/models/Notification.php
+++ frontend/models/Notification.php
@@ -2,7 +2,9 @@
 
 namespace app\models;
 
+use yii\db\ActiveRecord;
 use yii\helpers\ArrayHelper;
+use app\models\User;
 
 /**
  * This is the model class for table "notification".
@@ -11,14 +13,16 @@ use yii\helpers\ArrayHelper;
  * @property integer $id_user
  * @property string $name
  * @property integer $id_zakaz
+ * @property integer $todoist_id
  * @property integer $category
  * @property string $srok
  * @property integer $active
  *
+ * @property Todoist $todoist
  * @property Zakaz $idZakaz
  * @property User $idUser
  */
-class Notification extends \yii\db\ActiveRecord
+class Notification extends ActiveRecord
 {
     const ACTIVE = 1;
     const NOT_ACTIVE = 0;
@@ -41,10 +45,11 @@ class Notification extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id_user', 'category','active'], 'integer'],
+            [['id_user', 'category','todoist_id','active'], 'integer'],
             [['name'], 'string', 'max' => 50],
             [['srok'], 'safe'],
             [['id_zakaz'], 'exist', 'skipOnError' => true, 'targetClass' => Zakaz::className(), 'targetAttribute' => ['id_zakaz' => 'id_zakaz']],
+            [['todoist_id'], 'exist', 'skipOnError' => true, 'targetClass' => Todoist::className(), 'targetAttribute' => ['todoist_id' => 'id']],
             [['id_user'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['id_user' => 'id']],
         ];
     }
@@ -59,11 +64,22 @@ class Notification extends \yii\db\ActiveRecord
             'id_user' => 'Id User',
             'name' => 'Name',
             'id_zakaz' => 'Id Zakaz',
+            'todoist_id' => 'Todoist ID',
             'category' => 'Category',
             'srok' => 'Напоминание',
             'active' => 'Active',
         ];
     }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+
+    public function getTodoist()
+    {
+        return $this->hasOne(Todoist::className(), ['id' => 'todoist_id']);
+    }
+
 
     /**
      * @return \yii\db\ActiveQuery
@@ -85,6 +101,9 @@ class Notification extends \yii\db\ActiveRecord
 		$this->srok = null;
         $this->active = true;
         $this->save();
+        if (!$this->save()) {
+            print_r($this->getErrors());
+        }
     }
     public static function getCategoryArray()
     {
@@ -98,18 +117,29 @@ class Notification extends \yii\db\ActiveRecord
     {
         return ArrayHelper::getValue(self::getCategoryArray(), $this->category);
     }
+    public function getByIdNotificationTodoList($id, $todoist)
+    {
+        switch ($id) {
+            case 'create'://оформление уведомление исполнителю
+                $this->id_user = $todoist->id_user;
+                $this->name = 'Новая задача '.$todoist->comment;
+                $this->todoist_id = $todoist->id;
+                $this->category = 2;
+                break;
+            case 'ready'://оформление уведомление заказчика
+                $this->id_user = $todoist->id_sotrud_put;
+                $this->name = 'Выполнена задача '.$todoist->comment;
+                $this->todoist_id = $todoist->id;
+                $this->category = 2;
+                break;
+        }
+    }
     public function getByIdNotification($id, $zakaz)
     {
         switch ($id) {
-            case '7'://оформление уведомление доставки
-                $this->id_user = $id;
-                $this->name = 'Новая доставка №'.$zakaz;
-                $this->id_zakaz = $zakaz;
-                $this->category = 2;
-                break;
-            case '4'://оформление уведомление дизайнеру
-                $this->id_user = $id;
-                $this->name = 'Новый заказ №'.$zakaz;
+            case '2'://оформление уведомление администратору о назначение заказа
+                $this->id_user = User::USER_ADMIN;
+                $this->name = 'Магазин создал новый заказ №'.$zakaz;
                 $this->id_zakaz = $zakaz;
                 $this->category = 2;
                 break;
@@ -119,15 +149,45 @@ class Notification extends \yii\db\ActiveRecord
                 $this->id_zakaz = $zakaz;
                 $this->category = 2;
                 break;
+            case '4'://оформление уведомление дизайнеру
+                $this->id_user = $id;
+                $this->name = 'Новый заказ №'.$zakaz;
+                $this->id_zakaz = $zakaz;
+                $this->category = 2;
+                break;
+            case '5'://оформление уведомление выполение работы дизайнера
+                $this->id_user = $id;
+                $this->name = 'Дизайнер выполнил работу №'.$zakaz;
+                $this->id_zakaz = $zakaz;
+                $this->category = 1;
+                break;
+            case '6'://оформление уведомление выполение работы курьера
+                $this->id_user = $id;
+                $this->name = 'Курьер забрал заказ №'.$zakaz;
+                $this->id_zakaz = $zakaz;
+                $this->category = 1;
+                break;
+            case '7'://оформление уведомление доставки
+                $this->id_user = $id;
+                $this->name = 'Новая доставка №'.$zakaz;
+                $this->id_zakaz = $zakaz;
+                $this->category = 2;
+                break;
             case '8'://Уведомление, что мастер выполнил работу
                 $this->id_user = 5;
                 $this->name = 'Мастер выполнил работу №'.$zakaz;
                 $this->id_zakaz = $zakaz;
                 $this->category = 1;
                 break;
-            case '5'://оформление уведомление выполение работы дизайнера
+            case '9'://оформление уведомление курьер забрал заказ
                 $this->id_user = $id;
-                $this->name = 'Дизайнер выполнил работу №'.$zakaz;
+                $this->name = 'Курьер доставил заказ №'.$zakaz;
+                $this->id_zakaz = $zakaz;
+                $this->category = 1;
+                break;
+            case '10'://Уведомление, администратору что магазин закрыл заказ
+                $this->id_user = 5;
+                $this->name = 'Магазин закрыл заказ №'.$zakaz;
                 $this->id_zakaz = $zakaz;
                 $this->category = 1;
                 break;
