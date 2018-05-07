eb1b0d3da150ff61e6c50553a7dd2a9d2b851429
Silverfire <khabibulinemil@gmail.com>
2018-05-07 15:47:28 +0300

    Убрал нерабочий pjax

--- frontend/components/Notifications.php
+++ frontend/components/Notifications.php
@@ -6,25 +6,36 @@
  * Time: 14:15
  */
 namespace frontend\components;
+use app\models\User;
 use yii\base\Widget;
 use app\models\Notification;
 use Yii;
 use yii\helpers\Html;
-use yii\widgets\Pjax;
+
 
 
 class Notifications extends Widget
 {
     public function run()
     {
-        if (!Yii::$app->user->isGuest){
+        $notification = Notification::find();
+        if (Yii::$app->user->can('admin')){
+            $this->view->params['scoreNotification'] = $notification->andWhere(['active' => 1])->andWhere(['id_user' => 5 ])->count();
 
+         } elseif (Yii::$app->user->can('master')){
+            $this->view->params['scoreNotification'] = $notification->andWhere(['active' => 1])->andWhere(['id_user' => 4 ])->count();
+          } elseif (Yii::$app->user->can('disain')){
+            $this->view->params['scoreNotification'] = $notification->andWhere(['active' => 1])->andWhere(['id_user' => 3 ])->count();
+        }elseif (Yii::$app->user->can('shop')){
+            $this->view->params['scoreNotification'] = $notification->andWhere(['active' => 1])->andWhere(['id_user' => 9 ])->count();
+        }
+
+        if (!Yii::$app->user->isGuest){
             $notifications = Notification::find()->where(['id_user' => Yii::$app->user->id, 'active' => 1])->all();
 
             echo '<div class="notification">';
-            Pjax::begin(['id' => 'pjax-container']);
-            echo $notifications == null ? '<div class="notification-icon">' : '<div class="notification-icon newNotification">';
-            Pjax::end();
+
+            echo $notifications == null ? '<div class="notification-icon">' : '<div class="notification-icon newNotification">'.$this->view->params['scoreNotification']./*Yii::$app->session->addFlash('update', 'Появились новые уведомления');*/'';
             echo '<span class="glyphicon glyphicon-bell"></span>
                 </div>
                 <div class="notification-container hidden">
