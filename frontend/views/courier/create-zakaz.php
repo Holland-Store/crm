2bec53da937b296d5a1914bdb5774c4074e1e07e
Silverfire <khabibulinemil@gmail.com>
2018-05-11 10:01:13 +0300

    Замена формы ввода куда доставлять заказ на виджет select2 для возможности выбора выпадающим списком или ввода данных вручную

--- frontend/views/courier/create-zakaz.php
+++ frontend/views/courier/create-zakaz.php
@@ -2,7 +2,7 @@
 use kartik\widgets\DateTimePicker;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\select2\Select2;
 /** @var $shipping app\models\Courier */
 /** @var $model app\models\Zakaz */
 $this->registerJsFile('@web/js/yandexMap.js');
@@ -33,7 +33,22 @@ $this->registerJsFile('@web/js/yandexMap.js');
     <?= $f->field($shipping, 'to')->hiddenInput(['maxlength' => true, 'id' => 'toInput'])->label(false) ?>
     <?= $f->field($shipping, 'to_name')->hiddenInput(['maxlength' => true, 'id' => 'toName'])->label(false) ?>
 
-    <?= $f->field($shipping, 'fromYandexMap')->textInput(['placeholder' => 'Куда', 'id' => 'fromMap','class' => 'inputForm'])->label(false) ?>
+<!--    --><?/*= $f->field($shipping, 'fromYandexMap')->textInput(['placeholder' => 'Куда', 'id' => 'fromMap','class' => 'inputForm'])->label(false) */?>
+
+    <?=
+    $f->field($shipping, 'fromYandexMap')->widget(Select2::className(), [
+        'data' => [1 => "Московский", 2 => "Пушкина", 3 => "Маркса", 4 => "Четаева", 5 => "Новосибирский"],
+        'options' => ['placeholder' => 'Select a state ...'],
+        'pluginOptions' => [
+            'allowClear' => true,
+            'tags' => true,
+            'tokenSeparators' => [',', ' '],
+            'maximumInputLength' => 10
+        ],
+
+    ])->label(false);
+    ?>
+
     <?= $f->field($shipping, 'from')->hiddenInput(['maxlength' => true, 'id' => 'fromInput'])->label(false) ?>
     <?= $f->field($shipping, 'from_name')->hiddenInput(['maxlength' => true, 'id' => 'fromName'])->label(false) ?>
 
